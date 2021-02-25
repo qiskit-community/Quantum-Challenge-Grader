a1d556ddfd6086abfa621881b22a5f3e90e49ae6
va barbosa <vabarbosa@users.noreply.github.com>
2021-02-25 14:37:44 -0500

    exercise pre-check (#5)

--- qc_grader/exercises/qc/exercise1.py
+++ qc_grader/exercises/qc/exercise1.py
@@ -1,37 +1,62 @@
 from os import stat
 from qiskit import QuantumCircuit
 
-from qc_grader.grade import grade_circuit, submit_circuit
+from qc_grader.grade import grade_circuit, submit_circuit, handle_grade_response
+from qc_grader.exercises.qc.utils import precheck_exercise
 
 
 criteria: dict = {}
 
 
 def grade_ex1a(circuit: QuantumCircuit) -> None:
-    ok, _ = grade_circuit(circuit, 'ex1', 'partA', **criteria)
-    if ok:
-        print('Feel free to submit your answer.\r\n')
+    ok, msg = precheck_exercise(circuit, 1, 1)
+    if not ok:
+        handle_grade_response('invalid', cause=msg)
+    else:
+        ok, _ = grade_circuit(circuit, 'ex1', 'partA', **criteria)
+        if ok:
+            print('Feel free to submit your answer.\r\n')
 
 
 def grade_ex1b(circuit: QuantumCircuit) -> None:
-    ok, _ = grade_circuit(circuit, 'ex1', 'partB', **criteria)
-    if ok:
-        print('Feel free to submit your answer.\r\n')
+    ok, msg = precheck_exercise(circuit, 1, 2)
+    if not ok:
+        handle_grade_response('invalid', cause=msg)
+    else:
+        ok, _ = grade_circuit(circuit, 'ex1', 'partB', **criteria)
+        if ok:
+            print('Feel free to submit your answer.\r\n')
 
 
 def grade_ex1c(circuit: QuantumCircuit) -> None:
-    ok, _ = grade_circuit(circuit, 'ex1', 'partC', **criteria)
-    if ok:
-        print('Feel free to submit your answer.\r\n')
+    ok, msg = precheck_exercise(circuit, 1, 3)
+    if not ok:
+        handle_grade_response('invalid', cause=msg)
+    else:
+        ok, _ = grade_circuit(circuit, 'ex1', 'partC', **criteria)
+        if ok:
+            print('Feel free to submit your answer.\r\n')
 
 
 def submit_ex1a(circuit: QuantumCircuit) -> None:
-    submit_circuit(circuit, 'ex1', 'partA', **criteria)
+    ok, msg = precheck_exercise(circuit, 1, 1)
+    if not ok:
+        handle_grade_response('invalid', cause=msg)
+    else:
+        submit_circuit(circuit, 'ex1', 'partA', **criteria)
 
 
 def submit_ex1b(circuit: QuantumCircuit) -> None:
-    submit_circuit(circuit, 'ex1', 'partB', **criteria)
+    ok, msg = precheck_exercise(circuit, 1, 2)
+    if not ok:
+        handle_grade_response('invalid', cause=msg)
+    else:
+        submit_circuit(circuit, 'ex1', 'partB', **criteria)
 
 
 def submit_ex1c(circuit: QuantumCircuit) -> None:
-    submit_circuit(circuit, 'ex1', 'partC', **criteria)
+    ok, msg = precheck_exercise(circuit, 1, 3)
+    if not ok:
+        handle_grade_response('invalid', cause=msg)
+    else:
+        submit_circuit(circuit, 'ex1', 'partC', **criteria)
--- qc_grader/exercises/qc/exercise3.py
+++ qc_grader/exercises/qc/exercise3.py
@@ -1,8 +1,8 @@
-from typing import List, Optional
+from typing import Any, List, Optional
 
 import numpy as np
 
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import Aer, execute, QuantumCircuit, QuantumRegister
 from qiskit.circuit import Parameter
 
 
@@ -15,7 +15,38 @@ expected_circuit_count = 5
 max_qubits_count = 10
 
 
-def _quick_check_circuit(qc: QuantumCircuit, m: int) -> bool:
+
+def produce_unitary(m: int, beta: float) -> Any:
+    """Creates a unitary matrix for comparison with submissions.
+        Args:
+            m (int): Index of boolean function to create oracle for (see question).
+            beta (float): Angle as per question.
+            naux (int): Number of auxiliary (scratch) qubits.
+        Returns:
+            ndarray: 2**(m+naux) * 2**(m+naux) unitary matrix, with phase
+                     of first element == 0.
+    """
+    iden = np.identity(8, dtype=complex)
+    targets: dict = {
+        0: [],  # no solutions
+        1: ['111'],
+        2: ['111', '000'],
+        3: ['100', '010', '001'],
+        4: ['100', '010', '001', '111']
+    }
+
+    # Create Oracle Unitary
+    oracle_matrix = iden
+    for state in targets[m]:
+        idx = int(state, 2)
+        oracle_matrix[idx][idx] = np.exp(beta * 1j)
+
+    # Set phase of first element to 0 for comparison with submission
+    oracle_matrix = oracle_matrix * np.exp(-np.angle(oracle_matrix[0][0]) * 1j)
+    return oracle_matrix
+
+
+def _quick_check_circuit(qc: QuantumCircuit, m: int) -> List:
     """Check the circuit for expected parameters and input before sending to server
         for additional checks and grading.
         Args:
@@ -55,18 +86,35 @@ def _quick_check_circuit(qc: QuantumCircuit, m: int) -> bool:
         raise SubmissionError("You must name your three-qubit input register 'input'.")
 
     circuits = []
-
+    usim = Aer.get_backend('unitary_simulator')
+    
     for trials in range(30):
         b = np.random.rand()*np.pi*2
         param = {
             beta: b
         }
-        
-        circuits.append({
-            'beta': b,
-            'qc': circuit_to_json(qc, parameter_binds=[param])
-        })
-        
+
+        nqubits = len(qc.qubits)
+        if nqubits > max_qubits_count:
+            raise SubmissionError(f'Circuit cannot contain more than {max_qubits_count} qubits.')
+
+        submission = execute(qc, usim, parameter_binds=[param]).result().get_unitary()
+        submission = submission*np.exp(-np.angle(submission[0][0])*1j)
+        submission = submission[0:8, 0:8]  # all aux qubits should == |0>
+
+        is_close = np.isclose(submission, produce_unitary(m, param[beta]))
+
+        if not all(is_close.tolist()):
+            raise SubmissionError(
+                f"Circuit is incorrect (beta = {param[beta]})."
+            )
+        else:
+            circuits.append({
+                'beta': b,
+                'qc': circuit_to_json(qc, parameter_binds=[param])
+            })
+
+
     return circuits
 
 
