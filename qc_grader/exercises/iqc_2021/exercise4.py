00eb51e81df25149df83d82877f404ba24eff0f5
va barbosa <va@us.ibm.com>
2021-05-04 09:12:48 -0400

    update exercise

--- qc_grader/exercises/iqc_2021/exercise4.py
+++ qc_grader/exercises/iqc_2021/exercise4.py
@@ -1,14 +1,41 @@
 from qiskit import QuantumCircuit
 
-from qc_grader.grade import grade_qobj, submit_qobj
-from qiskit.qobj import PulseQobj
+from qc_grader.grade import grade_json, submit_json
 
 
-def grade_ex4(qobj: PulseQobj) -> None:
-    ok, _ = grade_qobj(qobj, 'ex4')
-    if ok:
-        print('Feel free to submit your answer.\r\n')
+def format_submission(
+    f12: float,
+    qubit_index: int,
+    backend_name: str
+) -> dict:
+    return {
+        'f12': f12,
+        'qubit_index': qubit_index,
+        'backend_name': backend_name
+    }
 
 
-def submit_ex4(qobj: PulseQobj) -> None:
-    submit_qobj(qobj, 'ex4')
+def grade_ex4(
+    f12: float,
+    qubit_index: int,
+    backend_name: str
+) -> None:
+    try:
+        submission = format_submission(f12, qubit_index, backend_name)
+        ok, _ = grade_json(submission, 'ex4')
+        if ok:
+            print('Feel free to submit your answer.\r\n')
+    except Exception as err:
+        print(err)
+
+
+def submit_ex4(
+    f12: float,
+    qubit_index: int,
+    backend_name: str
+) -> None:
+    try:
+        submission = format_submission(f12, qubit_index, backend_name)
+        submit_json(submission, 'ex4')
+    except Exception as err:
+        print(err)
