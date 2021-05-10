49017be8446461087dd1de3e903f51e3a26390bb
va barbosa <va@us.ibm.com>
2021-05-10 14:31:37 -0400

    update error messages

--- qc_grader/exercises/iqc_2021/exercise3.py
+++ qc_grader/exercises/iqc_2021/exercise3.py
@@ -1,6 +1,7 @@
 from typing import List
 from qiskit import QuantumCircuit
 
+from .. import SubmissionError
 from qc_grader.grade import grade_json, submit_json
 from qc_grader.util import circuit_to_json
 
@@ -8,12 +9,33 @@ from qc_grader.util import circuit_to_json
 criteria: dict = {}
 
 
+def verify_parameters(
+    qc_init: QuantumCircuit,
+    qc_syn: QuantumCircuit,
+    error_qubits: List[int],
+    initial_layout: dict
+):
+    if not isinstance(qc_init, QuantumCircuit):
+        raise SubmissionError("The 'qc_init' parameter must be of type QuantumCircuit")
+    if not isinstance(qc_syn, QuantumCircuit):
+        raise SubmissionError("The 'qc_syn' parameter must be of type QuantumCircuit")
+    if not isinstance(error_qubits, list) or len(error_qubits) == 0 or any(not isinstance(x, int) for x in error_qubits):
+        raise SubmissionError("The 'error_qubits' parameter must be a list of integers that map to qubit index values.")
+    if not isinstance(initial_layout, list) or len(initial_layout) == 0  or any(not isinstance(x, int) for x in initial_layout):
+        raise SubmissionError("The 'initial_layout' parameter must be a list of integers that map to qubit index values.")
+
 def format_submission(
     qc_init: QuantumCircuit,
     qc_syn: QuantumCircuit,
     error_qubits: List[int],
     initial_layout: dict
 ) -> dict:
+    verify_parameters(
+        qc_init,
+        qc_syn,
+        error_qubits,
+        initial_layout
+    )
     return {
         'qc_init': circuit_to_json(qc_init),
         'qc_syn': circuit_to_json(qc_syn),
@@ -38,6 +60,8 @@ def grade_ex3(
         ok, _ = grade_json(submission, 'ex3')
         if ok:
             print('Feel free to submit your answer.\r\n')
+    except SubmissionError as err:
+        print(err)
     except Exception as err:
         print(err)
 
@@ -56,5 +80,7 @@ def submit_ex3(
             initial_layout
         )
         submit_json(submission, 'ex3')
+    except SubmissionError as err:
+        print(err)
     except Exception as err:
         print(err)
