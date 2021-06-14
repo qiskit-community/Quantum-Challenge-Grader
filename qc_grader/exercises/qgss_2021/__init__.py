d3f226a649d4e9ddd0d20160baf0f0402322f9e5
va barbosa <vabarbosa@users.noreply.github.com>
2021-06-14 12:52:09 -0400

    updates (#8)
    * update grade and submit

---
+++ qc_grader/exercises/qgss_2021/__init__.py
@@ -0,0 +1,26 @@
+from .lab1 import (
+    grade_lab1_ex1, grade_lab1_ex2,
+    grade_lab1_ex3, grade_lab1_ex4,
+    grade_lab1_ex5, grade_lab1_ex6,
+    grade_lab1_ex7, grade_lab1_ex8
+)
+
+from .lab2 import (
+    grade_lab2_ex1, grade_lab2_ex2,
+    grade_lab2_ex3, grade_lab2_ex4
+)
+
+from .lab3 import (
+    grade_lab3_ex1, grade_lab3_ex2
+)
+
+from .lab4 import (
+    grade_lab4_ex1, grade_lab4_ex2,
+    grade_lab4_ex3, grade_lab4_ex4
+)
+
+from .lab5 import (
+    grade_lab5_ex1, grade_lab5_ex2,
+    grade_lab5_ex3, grade_lab5_ex4,
+    grade_lab5_ex5
+)
