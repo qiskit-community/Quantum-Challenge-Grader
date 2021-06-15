894f89df5e7ba15aea09de3bc5661df36b7ddd32
va barbosa <va@us.ibm.com>
2021-06-15 13:17:40 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,29 @@
 
 from setuptools import setup, find_packages
 
-from qc_grader import __version__
+import codecs
+import os.path
+
+
+# https://packaging.python.org/guides/single-sourcing-package-version/#single-sourcing-the-version
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
 
 setup(
     name='qc_grader',
-    version=__version__,
+    version=get_version('qc_grader/__init__.py'),
     description='Grading client for the IBM Quantum Challenge',
     url='https://quantum-computing.ibm.com/',
     author='IBM Quantum Community Team',
