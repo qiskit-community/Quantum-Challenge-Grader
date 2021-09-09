200c69bcbb7044841b2ccbddb5030249728b02f0
va barbosa <va@us.ibm.com>
2021-09-09 13:43:17 -0400

    show score

--- qc_grader/__init__.py
+++ qc_grader/__init__.py
@@ -8,7 +8,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-__version__ = '0.7.5'
+__version__ = '0.7.6'
 
 challenge_name = 'IBM Quantum Challenge Africa'
 challenge_version = '2021'
