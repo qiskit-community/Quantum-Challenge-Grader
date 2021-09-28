2c564e1d62a3870ace75584675c71b49689beb1d
Junye Huang <h.jun.ye@gmail.com>
2021-09-28 11:31:27 +0800

    starting ex3c validation and add jsonpickle to requirements.txt

--- qc_grader/exercises/iqc_2021_fall/__init__.py
+++ qc_grader/exercises/iqc_2021_fall/__init__.py
@@ -1,4 +1,5 @@
 from .challenge3 import (
     grade_ex3a,
-    grade_ex3b
+    grade_ex3b,
+    grade_ex3c
 )
--- qc_grader/exercises/iqc_2021_fall/challenge3.py
+++ qc_grader/exercises/iqc_2021_fall/challenge3.py
@@ -1,4 +1,7 @@
 from typing import Dict, Union
+
+import jsonpickle
+
 from qc_grader.grade import grade_and_submit
 
 def grade_ex3a(answer: Dict) -> None:
@@ -6,3 +9,7 @@ def grade_ex3a(answer: Dict) -> None:
 
 def grade_ex3b(answer: Dict) -> None:
     grade_and_submit(answer, 'ex3', 'partB')
+
+def grade_ex3c(numpy_results: ElectronicStructureResult) -> None:
+    answer = jsonpickle.encode(numpy_results)
+    grade_and_submit(answer, 'ex3', 'partC')
