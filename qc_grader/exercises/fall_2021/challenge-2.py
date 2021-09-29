68402dc1ec3dafbd8ddb06a44b180204d7fa5c26
Junye Huang <h.jun.ye@gmail.com>
2021-09-29 11:29:28 +0800

    rename challenge3.py to challenge-2.py

---
+++ qc_grader/exercises/fall_2021/challenge-2.py
@@ -0,0 +1,17 @@
+from typing import Dict, Union
+
+from qiskit_nature.results.electronic_structure_result import ElectronicStructureResult
+
+import jsonpickle
+
+from qc_grader.grade import grade_and_submit
+
+def grade_ex2a(answer: Dict) -> None:
+    grade_and_submit(answer, 'ex2', 'partA')
+
+def grade_ex2b(answer: Dict) -> None:
+    grade_and_submit(answer, 'ex2', 'partB')
+
+def grade_ex2c(numpy_results: ElectronicStructureResult) -> None:
+    answer = jsonpickle.encode(numpy_results)
+    grade_and_submit(answer, 'ex2', 'partC')
