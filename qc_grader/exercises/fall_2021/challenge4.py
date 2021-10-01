6f89c15c38242f8c59638d3335c301eeb97d822f
Junye Huang <h.jun.ye@gmail.com>
2021-10-01 14:46:40 +0800

    add ex4a validation

--- qc_grader/exercises/fall_2021/challenge4.py
+++ qc_grader/exercises/fall_2021/challenge4.py
@@ -1,13 +1,20 @@
 from typing import Dict, Union
 import jsonpickle
 
-from qiskit_finance.applications.optimization.portfolio_optimization import PortfolioOptimization
+from qiskit_optimization.problems.quadratic_program import QuadraticProgram
 from qiskit_optimization.algorithms.minimum_eigen_optimizer import MinimumEigenOptimizationResult
 
 from qc_grader.grade import grade_and_submit
 
-def grade_ex4a(portfolio: PortfolioOptimization) -> None:
-    answer = jsonpickle.encode(portfolio)
+def grade_ex4a(quadratic_program: QuadraticProgram) -> None:
+    objective = quadratic_program.objective
+
+    answer_dict = {
+                'offset': objective.constant,
+                'matrix': objective.quadratic.to_array(symmetric=True),
+                'linear coeff': objective.linear.to_array()
+            }
+    answer = jsonpickle.encode(answer_dict)
     grade_and_submit(answer, 'ex4', 'partA')
 
 def grade_ex4b(result: MinimumEigenOptimizationResult) -> None:
