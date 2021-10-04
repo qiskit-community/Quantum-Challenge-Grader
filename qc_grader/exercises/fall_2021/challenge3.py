1765ac0fc0b0ca67c1a59f5ea37d1a424f42170b
Junye Huang <h.jun.ye@gmail.com>
2021-10-04 16:07:11 +0800

    ex3a validation in progress

--- qc_grader/exercises/fall_2021/challenge3.py
+++ qc_grader/exercises/fall_2021/challenge3.py
@@ -1,7 +1,21 @@
 from typing import Any
+import jsonpickle
+import numpy as np
+
+from qiskit.circuit.library import NLocal
 
 from qc_grader.grade import grade_and_submit
+from qc_grader.util import circuit_to_json
 
 
-def grade_ex3a(answer: Any) -> None:
-    print('Grading not yet available')
+def grade_ex3a(pred_test: np.ndarray, fmap_1: NLocal, fmap_2: NLocal, fmap_3: NLocal, n_dim: int) -> None:
+    answer_dict = {
+            'pred_test': pred_test,
+            'fmap_1': circuit_to_json(fmap_1),
+            'fmap_2': circuit_to_json(fmap_2),
+            'fmap_3': circuit_to_json(fmap_3),
+            'n_dim': n_dim
+    }
+    answer = jsonpickle.encode(answer_dict)
+
+    grade_and_submit(answer, 'ex3', 'partA')
