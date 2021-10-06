872ea1f8a950e6e2eb22d7869fcc9b585fab63e8
Junye Huang <h.jun.ye@gmail.com>
2021-10-06 11:49:16 +0800

    use pickling

--- qc_grader/exercises/fall_2021/challenge3.py
+++ qc_grader/exercises/fall_2021/challenge3.py
@@ -1,4 +1,5 @@
 from typing import Any
+import pickle
 import jsonpickle
 import numpy as np
 
@@ -11,11 +12,11 @@ from qc_grader.util import circuit_to_json
 def grade_ex3a(pred_test: np.ndarray, fmap_1: NLocal, fmap_2: NLocal, fmap_3: NLocal, n_dim: int) -> None:
     answer_dict = {
             'pred_test': pred_test,
-            'fmap_1': circuit_to_json(fmap_1),
-            'fmap_2': circuit_to_json(fmap_2),
-            'fmap_3': circuit_to_json(fmap_3),
+            'fmap_1': fmap_1,
+            'fmap_2': fmap_2,
+            'fmap_3': fmap_3,
             'n_dim': n_dim
     }
-    answer = jsonpickle.encode(answer_dict)
+    answer = pickle.dumps(answer_dict).decode('ISO-8859-1')
 
     grade_and_submit(answer, 'ex3', 'partA')
