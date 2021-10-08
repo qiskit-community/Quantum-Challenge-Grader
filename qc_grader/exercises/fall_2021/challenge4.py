8e042843390d99e56b6fccc79e96b045a0efc68d
Junye Huang <h.jun.ye@gmail.com>
2021-10-08 12:33:33 +0800

    move valdation to server

--- qc_grader/exercises/fall_2021/challenge4.py
+++ qc_grader/exercises/fall_2021/challenge4.py
@@ -1,6 +1,7 @@
 from typing import Any, Callable
 from typeguard import typechecked
 import timeout_decorator
+import pickle
 
 from qiskit import Aer
 from qiskit.algorithms import QAOA
@@ -39,7 +40,6 @@ def grade_ex4b(function: Callable) -> None:
     msg = ""
 
     problem_set_index = answer_dict['index']
-    L1, L2, C1, C2, C_max = answer_dict['problem-set'].values()
     values, weights, max_weight = answer_dict['result']
 
     
@@ -54,38 +54,16 @@ def grade_ex4b(function: Callable) -> None:
     qaoa_mes = QAOA(quantum_instance = qins, reps = 2)
     qaoa = MinimumEigenOptimizer(qaoa_mes)
 
-    try:
-        result = solve_qaoa(qp, qaoa)
-    except:
-        false_inputs.append(problem_set_index)
-        msg += "In " + str(problem_set_index + 1) + " th input, your circuit exeeded the limit of execution time: " + str(time_limit_4b) + " s\n"
-    else:
-        ans = ""
-        for j in list(result.x.astype("int64")):
-            ans += str(j)
-        if not is_feasible(L1, L2, C1, C2, C_max, ans):
-            false_inputs.append(problem_set_index)
-            msg += "Infeasible answer for input no." + str(problem_set_index + 1) + "\n"
-    print(msg) 
-    print('Correct answer!')
-    #if answer:
-    #    grade_and_submit(answer, 'ex4', 'partB')
+    result = qaoa.solve(qp)
 
-#@timeout_decorator.timeout(time_limit_4b)
-def solve_qaoa(qp, qaoa):
-    return qaoa.solve(qp)
+    answer_dict = {
+            'index': problem_set_index,
+            'result': result
+            }
+    answer = pickle.dumps(answer_dict).decode('ISO-8859-1')
 
-def is_feasible(L1, L2, C1, C2, C_max, ans):
-    value = 0
-    cost = 0
-    for i, a in enumerate(ans):
-        if a == "0":
-            value += L1[i]
-            cost += C1[i]
-        else:
-            value += L2[i]
-            cost += C2[i]
-    return True if cost <= C_max else False
+    grade_and_submit(answer, 'ex4', 'partB')
+
 
 @typechecked
 def grade_ex4c(answer: Any) -> None:
