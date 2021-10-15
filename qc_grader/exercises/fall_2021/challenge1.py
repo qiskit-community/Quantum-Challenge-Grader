76729fd4505e5c5ee0588e540cd8576220e004b1
Junye Huang <h.jun.ye@gmail.com>
2021-10-15 14:55:51 +0800

    ex1 updates

--- qc_grader/exercises/fall_2021/challenge1.py
+++ qc_grader/exercises/fall_2021/challenge1.py
@@ -1,9 +1,10 @@
+import pickle
 import jsonpickle
-import numpy.typing as npt
 
 from typeguard import typechecked
 
 from qiskit_optimization import QuadraticProgram
+from qiskit_optimization.algorithms.minimum_eigen_optimizer import MinimumEigenOptimizationResult
 
 from qc_grader.grade import grade_and_submit
 
@@ -15,12 +16,18 @@ def grade_ex1a(qp: QuadraticProgram) -> None:
 
 
 @typechecked
-def grade_ex1b(numpy_results: npt.ArrayLike) -> None:
-    answer = jsonpickle.encode(numpy_results)
+def grade_ex1b(result: MinimumEigenOptimizationResult) -> None:
+    answer = pickle.dumps(result).decode('ISO-8859-1')
     grade_and_submit(answer, '1b')
 
 
 @typechecked
-def grade_ex1c(numpy_results: npt.ArrayLike) -> None:
-    answer = jsonpickle.encode(numpy_results)
+def grade_ex1c(qp: QuadraticProgram) -> None:
+    answer = jsonpickle.encode(qp.export_as_lp_string())
     grade_and_submit(answer, '1c')
+
+
+@typechecked
+def grade_ex1d(result: MinimumEigenOptimizationResult) -> None:
+    answer = pickle.dumps(result).decode('ISO-8859-1')
+    grade_and_submit(answer, '1d')
