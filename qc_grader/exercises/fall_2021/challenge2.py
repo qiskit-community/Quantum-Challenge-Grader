f85cbca5120d90a38e15b9b677528b2a689dc459
Junye Huang <h.jun.ye@gmail.com>
2021-10-19 12:43:44 +0800

    direct grading runtime program

--- qc_grader/exercises/fall_2021/challenge2.py
+++ qc_grader/exercises/fall_2021/challenge2.py
@@ -2,6 +2,11 @@ from typing import Any, Dict
 from typeguard import typechecked
 
 from qiskit_nature.results.electronic_structure_result import ElectronicStructureResult
+from qiskit_nature.runtime import VQEProgram
+from qiskit_nature.converters.second_quantization import QubitConverter
+from qiskit_nature.problems.second_quantization.electronic import ElectronicStructureProblem
+from qiskit.providers.ibmq.job import IBMQJob
+from qiskit_nature.algorithms.ground_state_solvers import GroundStateEigensolver
 
 import jsonpickle
 
@@ -37,6 +42,17 @@ def grade_ex2e(result: ElectronicStructureResult) -> None:
 
 
 @typechecked
-def grade_ex2f(answer: Any) -> None:
-    print('Grading not yet available')
-    # grade_and_submit(answer, '2f')
+def grade_ex2f(
+        runtime_vqe: VQEProgram,
+        qubit_converter: QubitConverter,
+        problem: ElectronicStructureProblem
+) -> None:
+    # check provider
+    # check backend
+    # create ground state solver
+    runtime_gse = GroundStateEigensolver(qubit_converter, runtime_vqe)
+    print('Running VQE Runtime job, this can take a few minutes.')
+    runtime_vqe_results = runtime_gse.solve(problem)
+
+    answer = jsonpickle.encode(runtime_vqe_results)
+    grade_and_submit(answer, '2f')
