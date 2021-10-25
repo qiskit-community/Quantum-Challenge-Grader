97cfdb75ab573f1ac2317d999c4e3c2c4dd8f5d6
Junye Huang <h.jun.ye@gmail.com>
2021-10-25 12:06:45 +0800

    use VQE for ex1b and QAOA for ex1d

--- qc_grader/exercises/fall_2021/challenge1.py
+++ qc_grader/exercises/fall_2021/challenge1.py
@@ -3,8 +3,9 @@ import jsonpickle
 
 from typeguard import typechecked
 
+from qiskit.algorithms.minimum_eigen_solvers import VQE, QAOA
 from qiskit_optimization import QuadraticProgram
-from qiskit_optimization.algorithms.minimum_eigen_optimizer import MinimumEigenOptimizationResult
+from qiskit_optimization.algorithms.minimum_eigen_optimizer import MinimumEigenOptimizer
 
 from qc_grader.grade import grade_and_submit
 
@@ -16,7 +17,9 @@ def grade_ex1a(qp: QuadraticProgram) -> None:
 
 
 @typechecked
-def grade_ex1b(result: MinimumEigenOptimizationResult) -> None:
+def grade_ex1b(vqe: VQE, qp: QuadraticProgram) -> None:
+    meo = MinimumEigenOptimizer(vqe)
+    result = meo.solve(qp)
     answer = pickle.dumps(result).decode('ISO-8859-1')
     grade_and_submit(answer, '1b')
 
@@ -28,6 +31,8 @@ def grade_ex1c(qp: QuadraticProgram) -> None:
 
 
 @typechecked
-def grade_ex1d(result: MinimumEigenOptimizationResult) -> None:
+def grade_ex1d(qaoa: QAOA, qp: QuadraticProgram) -> None:
+    meo = MinimumEigenOptimizer(qaoa)
+    result = meo.solve(qp)
     answer = pickle.dumps(result).decode('ISO-8859-1')
     grade_and_submit(answer, '1d')
