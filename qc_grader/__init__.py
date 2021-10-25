2d4483f4f7783b0a64b118b47f01526904c0d8a0
Junye Huang <h.jun.ye@gmail.com>
2021-10-25 23:36:55 +0800

    reduce version number to 0.8.4

--- qc_grader/__init__.py
+++ qc_grader/__init__.py
@@ -11,7 +11,7 @@
 import warnings
 warnings.filterwarnings("ignore")
 
-__version__ = '0.8.5'
+__version__ = '0.8.4'
 
 challenge_name = 'IBM Quantum Challenge Fall 2021'
 challenge_version = '2021'
