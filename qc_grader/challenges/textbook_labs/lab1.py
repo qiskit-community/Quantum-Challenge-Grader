d7b8b1776403f2d7f9c9a9c012f4a6e1d613bab9
David García <9059044+Tansito@users.noreply.github.com>
2022-02-18 10:53:38 +0100

    Added textbook_labs

---
+++ qc_grader/challenges/textbook_labs/lab1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# (C) Copyright IBM 2022
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from pathlib import Path
+from typeguard import typechecked
+
+challenge_id = Path(__file__).parent.name
+
+@typechecked
+def grade_lab1(answer: str) -> None:
+    grade(answer, 1, challenge_id)
