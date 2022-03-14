52c06b4f4f74c9d5d81706a04a4327c7bdb4fec2
Sanket Panda <sanket@ibm.com>
2022-03-14 11:20:02 -0400

    Add Intro Practice Exam grader functions

---
+++ qc_grader/challenges/intro_practice_exam/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# (C) Copyright IBM 2022
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from .intro_practice_exam import (
+    grade_problem1c,
+    grade_problem2a,
+    grade_problem2b,
+    grade_problem2c,
+)
---
+++ qc_grader/challenges/intro_practice_exam/intro_practice_exam.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# (C) Copyright IBM 2022
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from pathlib import Path
+from typeguard import typechecked
+from typing import Any, Dict
+
+from qc_grader.grader.grade import grade
+
+challenge_id = Path(__file__).parent.name
+
+
+@typechecked
+def grade_problem1c(answer: Dict[str, Any]) -> None:
+    grade(answer, 1, challenge_id)  # 1c
+
+
+@typechecked
+def grade_problem2a(answer: Dict[str, Any]) -> None:
+    grade(answer, 2, challenge_id)  # 2a
+
+
+@typechecked
+def grade_problem2b(answer: Dict[str, Any]) -> None:
+    grade(answer, 3, challenge_id)  # 2b
+
+
+@typechecked
+def grade_problem2c(answer: Dict[str, Any]) -> None:
+    grade(answer, 4, challenge_id)  # 2c
