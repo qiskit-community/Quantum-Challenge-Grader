595ec3ea0d78190638be206036202e51f24156b0
va barbosa <va@us.ibm.com>
2022-05-09 17:41:58 -0400

    update functions

--- qc_grader/challenges/spring_2022/exercise2.py
+++ qc_grader/challenges/spring_2022/exercise2.py
@@ -6,18 +6,18 @@ from qiskit import QuantumCircuit
 from qc_grader.grader.grade import grade
 
 
-_challenge_id = 'spring-2022'
+_challenge_id = 'spring_2022'
 
 
 @typechecked
 def grade_ex2a(qc: QuantumCircuit) -> None:
-    grade(qc, '2a', _challenge_id, byte_string=True)
+    grade(qc, '2a', _challenge_id, do_submit=True, byte_string=True)
 
 @typechecked
 def grade_ex2b(prob_densities: List[List[float]]) -> None:
-    grade(prob_densities, '2b', _challenge_id)
+    grade(prob_densities, '2b', _challenge_id, do_submit=True)
 
 
 @typechecked
 def grade_ex2c(prob_densitiy_localization: List[List[float]]) -> None:
-    grade(prob_densitiy_localization, '2c', _challenge_id)
+    grade(prob_densitiy_localization, '2c', _challenge_id, do_submit=True)
