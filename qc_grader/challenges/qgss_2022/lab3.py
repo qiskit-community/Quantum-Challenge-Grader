a3047c52cad9d05765c7053dd7ec84485d1c9a8a
va barbosa <va@us.ibm.com>
2022-06-02 16:52:10 -0400

    Update lab3.py

--- qc_grader/challenges/qgss_2022/lab3.py
+++ qc_grader/challenges/qgss_2022/lab3.py
@@ -10,13 +10,13 @@ _challenge_id = 'qgss_2022'
 
 @typechecked
 def grade_lab3_ex1(qc: QuantumCircuit) -> None:
-    grade(qc, 'ex3-1', _challenge_id, byte_string=True)
+    grade(qc, 'ex3-1', _challenge_id)
 
 
 @typechecked
 def grade_lab3_ex2(zero_state_count: int, one_state_count: int) -> None:
     answer = {
-        'zeroState': zero_state_count,
-        'oneState': one_state_count
+        'zero_state': zero_state_count,
+        'one_state': one_state_count
     }
     grade(answer, 'ex3-2', _challenge_id)
