fc10b40d2af7fd253cfd579b44b2ae5a7147fe03
David <9059044+Tansito@users.noreply.github.com>
2022-06-02 17:14:14 +0200

    Initial setup for QGSS 2022

---
+++ qc_grader/challenges/qgss_2022/__init__.py
@@ -0,0 +1,15 @@
+from .lab1 import (
+    grade_lab1_ex1,
+    grade_lab1_ex2,
+    grade_lab1_ex3,
+    grade_lab1_ex4,
+    grade_lab1_ex5,
+    grade_lab1_ex6,
+    grade_lab1_ex7,
+    grade_lab1_ex8
+)
+
+from .lab3 import (
+    grade_lab3_ex1,
+    grade_lab3_ex2
+)
---
+++ qc_grader/challenges/qgss_2022/lab1.py
@@ -0,0 +1,57 @@
+from typeguard import typechecked
+
+from typing import Callable
+
+from qiskit import QuantumCircuit
+
+from qc_grader.grader.grade import grade
+
+
+_challenge_id = 'qgss_2022'
+
+
+@typechecked
+def grade_lab1_ex1(qc: QuantumCircuit) -> None:
+    grade(qc, 'ex1-1', _challenge_id, byte_string=True)
+
+
+@typechecked
+def grade_lab1_ex2(qc: QuantumCircuit) -> None:
+    grade(qc, 'ex1-2', _challenge_id, byte_string=True)
+
+
+@typechecked
+def grade_lab1_ex3(qc: QuantumCircuit) -> None:
+    grade(qc, 'ex1-3', _challenge_id, byte_string=True)
+
+
+@typechecked
+def grade_lab1_ex4(qc: QuantumCircuit) -> None:
+    grade(qc, 'ex1-4', _challenge_id, byte_string=True)
+
+
+@typechecked
+def grade_lab1_ex5(qc: QuantumCircuit) -> None:
+    grade(qc, 'ex1-5', _challenge_id, byte_string=True)
+
+
+@typechecked
+def grade_lab1_ex6(qc: QuantumCircuit) -> None:
+    grade(qc, 'ex1-6', _challenge_id, byte_string=True)
+
+
+@typechecked
+def grade_lab1_ex7(numberOfGatesFnc: Callable) -> None:
+    inputs = get_problem_set('ex1-7', _challenge_id)
+
+    answer = []
+    for i in inputs:
+        result = numberOfGatesFnc(i)
+        answer.append((i, result))
+
+    grade(answer, 'ex1-7', _challenge_id, max_content_length=2*1024*1024)
+
+
+@typechecked
+def grade_lab1_ex8(qc: QuantumCircuit) -> None:
+    grade(qc, 'ex1-8', _challenge_id, byte_string=True)   
