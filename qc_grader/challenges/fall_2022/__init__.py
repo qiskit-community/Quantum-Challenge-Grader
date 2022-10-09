e8eb999205d8a988e4f0c6bf80ca7345a7e39126
Esperanza Molina <108661074+memolina2323@users.noreply.github.com>
2022-10-09 09:25:52 +0200

    First validations (#61)
    * fall 2022 first commits
    
    * updated lab 1
    
    * init changes
    
    * include SamplerResult serialization

--- qc_grader/challenges/fall_2022/__init__.py
+++ qc_grader/challenges/fall_2022/__init__.py
@@ -4,10 +4,7 @@ from .lab1 import (
     grade_lab1_ex3,
     grade_lab1_ex4,
     grade_lab1_ex5,
-    grade_lab1_ex6,
-    grade_lab1_ex7,
-    grade_lab1_ex8,
-    grade_lab1_ex9
+    grade_lab1_ex6
 )
 
 from .lab2 import (
@@ -15,8 +12,7 @@ from .lab2 import (
     grade_lab2_ex2,
     grade_lab2_ex3,
     grade_lab2_ex4,
-    grade_lab2_ex5,
-    grade_lab2_ex6
+    grade_lab2_ex5
 )
 
 from .lab3 import (
