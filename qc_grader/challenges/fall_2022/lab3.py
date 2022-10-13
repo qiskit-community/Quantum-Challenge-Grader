509330e61d187f43948321d7d8d01091d24b056b
Maria Esperanza Molina Canizares <Maria.Esperanza.Molina.Canizares@ibm.com>
2022-10-13 16:55:12 +0200

    update lab3 client

--- qc_grader/challenges/fall_2022/lab3.py
+++ qc_grader/challenges/fall_2022/lab3.py
@@ -1,33 +1,22 @@
 from typeguard import typechecked
+
+from typing import Callable
+
 from qiskit import QuantumCircuit
+from qiskit.result import QuasiDistribution
 from qc_grader.grader.grade import grade, get_problem_set
+
 
 _challenge_id = 'fall_2022'
 
 @typechecked
-def grade_lab3_ex1(qc: QuantumCircuit) -> None:
-    grade(qc, 'ex3-1', _challenge_id)
+def grade_lab3_ex1(attempt_qc: QuantumCircuit) -> None:
+    grade(attempt_qc: QuantumCircuit, 'ex3-1', _challenge_id)
 
 @typechecked
-def grade_lab3_ex2(qc: QuantumCircuit) -> None:
-    grade(qc, 'ex3-2', _challenge_id)
+def grade_lab3_ex2(attempt: QuasiDistribution) -> None:
+    grade(attempt: QuasiDistribution, 'ex3-2', _challenge_id)
 
 @typechecked
-def grade_lab3_ex3(qc: QuantumCircuit) -> None:
-    grade(qc, 'ex3-3', _challenge_id)
-
-@typechecked
-def grade_lab3_ex4(qc: QuantumCircuit) -> None:
-    grade(qc, 'ex3-4', _challenge_id)
-
-@typechecked
-def grade_lab3_ex5(qc: QuantumCircuit) -> None:
-    grade(qc, 'ex3-5', _challenge_id)
-
-@typechecked
-def grade_lab3_ex6(qc: QuantumCircuit) -> None:
-    grade(qc, 'ex3-6', _challenge_id)
-
-@typechecked
-def grade_lab3_ex7(qc: QuantumCircuit) -> None:
-    grade(qc, 'ex3-7', _challenge_id)
+def grade_lab3_ex3(attempt_etgl: function) -> None:
+    grade(attempt_etgl: function, 'ex3-3', _challenge_id)
