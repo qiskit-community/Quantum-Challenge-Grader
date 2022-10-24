8be7dd484dbf7182c842749678e61815368203dd
alexao8 <alex.menorca@gmail.com>
2022-10-24 15:25:35 +0300

    serializing lab3

--- qc_grader/challenges/fall_2022/lab3.py
+++ qc_grader/challenges/fall_2022/lab3.py
@@ -14,7 +14,7 @@ from qiskit.primitives import SamplerResult
 from qiskit.result import ProbDistribution, QuasiDistribution
 
 from qc_grader.grader.grade import grade
-from qc_grader.grader.common import graph_to_json, circuit_to_json
+from qc_grader.grader.common import graph_to_json, circuit_to_json, paulisumop_to_json, quasidistribution_to_json, vqeresult_to_json, samplerresult_to_json, probdistribution_to_json
 
 
 _challenge_id = 'fall_2022'
@@ -30,17 +30,20 @@ def grade_lab3_ex1(attempt_graph: Graph, attempt_n: int) -> None:
 
 @typechecked
 def grade_lab3_ex2(attempt_ising: PauliSumOp) -> None:
-    grade(attempt_ising, 'ex3-2', _challenge_id)
+    answer = paulisumop_to_json(attempt_ising)
+    grade(answer, 'ex3-2', _challenge_id)
 
 
 @typechecked
 def grade_lab3_ex3(attempt_qc: QuantumCircuit) -> None:
-    grade(attempt_qc, 'ex3-3', _challenge_id)
+    answer = circuit_to_json(attempt_qc)
+    grade(answer, 'ex3-3', _challenge_id)
 
 
 @typechecked
 def grade_lab3_ex4(attempt: QuasiDistribution) -> None:
-    grade(attempt, 'ex3-4', _challenge_id)
+    answer = quasidistribution_to_json(attempt)
+    grade(answer, 'ex3-4', _challenge_id)
 
 
 @typechecked
@@ -56,7 +59,8 @@ def grade_lab3_ex5(attempt_etgl: Callable) -> None:
 
 @typechecked
 def grade_lab3_ex6(result: VQEResult) -> None:
-    grade(result, 'ex3-6', _challenge_id)
+    answer = vqeresult_to_json(VQEResult)
+    grade(answer, 'ex3-6', _challenge_id)
 
 
 @typechecked
@@ -64,14 +68,26 @@ def grade_lab3_ex7(
     sampler_result: SamplerResult,
     result_prob_dist: ProbDistribution
 ) -> None:
-    grade(sampler_result, result_prob_dist, 'ex3-7', _challenge_id) 
+    answer = {
+        'sampler_result': samplerresult_to_json(sampler_result),
+        'result_prob_dist': probdistribution_to_json(result_prob_dist)
+    }
+    grade(answer, 'ex3-7', _challenge_id) 
 
 
 @typechecked
 def grade_lab3_ex8(model_3: QuantumCircuit, result_m3: VQEResult) -> None:
-    grade(model_3, result_m3, 'ex3-8', _challenge_id) 
+    answer = {
+        'qc': circuit_to_json(model_3),
+        'vqeresult': vqeresult_to_json(result_m3)
+    }
+    grade(answer, 'ex3-8', _challenge_id)
 
 
 @typechecked
 def grade_lab3_ex9(vqe_result: VQEResult, bitstring: ndarray) -> None:
-    grade(vqe_result, bitstring, 'ex3-9', _challenge_id) 
+    answer = {
+        'vqe_result': vqeresult_to_json(vqe_result),
+        'bitstring': bitstring
+   }
+    grade(answer, bitstring, 'ex3-9', _challenge_id) 
