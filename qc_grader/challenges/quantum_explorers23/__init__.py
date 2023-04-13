87694c20f546f26befc3399a704b9c5aa294091f
va barbosa <va@us.ibm.com>
2023-04-13 13:21:59 -0400

    qe23 skeleton

---
+++ qc_grader/challenges/quantum_explorers23/__init__.py
@@ -0,0 +1,3 @@
+from .badge_1 import grade_badge1_ex1
+
+from .badge_2 import grade_badge2_ex1
---
+++ qc_grader/challenges/quantum_explorers23/badge_1.py
@@ -0,0 +1,18 @@
+from typing import List
+from typeguard import typechecked
+
+from qc_grader.grader.grade import grade
+
+
+_challenge_id = 'quantum_explorers23'
+
+
+@typechecked
+def grade_badge1_ex1(answer: List[int]) -> None:
+    status, _, message = grade(
+        answer,
+        'badge1_ex1',
+        _challenge_id, 
+        return_response=True
+    )
+    print('Thank you for submitting your answer')
