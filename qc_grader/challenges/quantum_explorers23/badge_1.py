f586665e5bcdb4fc902431ca6ec3318103717f71
quantum-kittens <14252649+quantum-kittens@users.noreply.github.com>
2023-04-14 21:13:10 +0530

    Add grader functions for badge 1 questions 2 and 3

--- qc_grader/challenges/quantum_explorers23/badge_1.py
+++ qc_grader/challenges/quantum_explorers23/badge_1.py
@@ -16,3 +16,25 @@ def grade_badge1_ex1(answer: List[int]) -> None:
         return_response=True
     )
     print('Thank you for submitting your answer')
+    
+    
+@typechecked
+def grade_badge1_ex2(answer: List[int]) -> None:
+    status, _, message = grade(
+        answer,
+        'badge1_ex2',
+        _challenge_id, 
+        return_response=True
+    )
+    print('Thank you for submitting your answer')
+    
+
+@typechecked
+def grade_badge1_ex3(answer: List[int]) -> None:
+    status, _, message = grade(
+        answer,
+        'badge1_ex3',
+        _challenge_id, 
+        return_response=True
+    )
+    print('Thank you for submitting your answer')
