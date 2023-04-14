f5488361f473a6028d69f33ec9841786428dd95e
quantum-kittens <14252649+quantum-kittens@users.noreply.github.com>
2023-04-14 22:24:01 +0530

    Add score question grader function

--- qc_grader/challenges/quantum_explorers23/badge_1.py
+++ qc_grader/challenges/quantum_explorers23/badge_1.py
@@ -38,3 +38,13 @@ def grade_badge1_ex3(answer: List[int]) -> None:
         return_response=True
     )
     print('Thank you for submitting your answer')
+    
+    
+@typechecked
+def grade_badge1_score(answer: List[int]) -> None:
+    status, _, message = grade(
+        answer,
+        'badge1_score',
+        _challenge_id, 
+        return_response=True
+    )
