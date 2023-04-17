06bc86f900c232da5d79b2c64a4317f0e3ace52f
quantum-kittens <14252649+quantum-kittens@users.noreply.github.com>
2023-04-17 22:36:10 +0530

    Add grader functions for 2, 3 and score checker + update answer variables (#94)
    * Add grader functions for badge 1 questions 2 and 3
    
    * Add score question grader function
    
    * Update answer variables
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    ---------
    
    Co-authored-by: //va <vabarbosa@users.noreply.github.com>

--- qc_grader/challenges/quantum_explorers23/badge_1.py
+++ qc_grader/challenges/quantum_explorers23/badge_1.py
@@ -8,11 +8,43 @@ _challenge_id = 'quantum_explorers23'
 
 
 @typechecked
-def grade_badge1_ex1(answer: List[int]) -> None:
+def grade_badge1_ex1(answer1: List[int]) -> None:
     status, _, message = grade(
-        answer,
+        answer1,
         'badge1_ex1',
         _challenge_id, 
         return_response=True
     )
     print('Thank you for submitting your answer')
+    
+    
+@typechecked
+def grade_badge1_ex2(answer2: List[int]) -> None:
+    status, _, message = grade(
+        answer2,
+        'badge1_ex2',
+        _challenge_id, 
+        return_response=True
+    )
+    print('Thank you for submitting your answer')
+    
+
+@typechecked
+def grade_badge1_ex3(answer3: List[int]) -> None:
+    status, _, message = grade(
+        answer3,
+        'badge1_ex3',
+        _challenge_id, 
+        return_response=True
+    )
+    print('Thank you for submitting your answer')
+    
+    
+@typechecked
+def grade_badge1_score(check_score: List[int]) -> None:
+    status, _, message = grade(
+        check_score,
+        'badge1_score',
+        _challenge_id, 
+        return_response=True
+    )
