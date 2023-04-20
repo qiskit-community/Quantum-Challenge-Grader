f9ff2eda4aff17a3a826d15904e2c987f7e7d8f5
va barbosa <va@us.ibm.com>
2023-04-19 22:26:58 -0400

    Update __init__.py

--- qc_grader/challenges/quantum_explorers23/__init__.py
+++ qc_grader/challenges/quantum_explorers23/__init__.py
@@ -1,3 +1,10 @@
-from .badge_1 import grade_badge1_ex1
+from .badge_1 import (
+    grade_badge1_ex1,
+    grade_badge1_ex2,
+    grade_badge1_ex3,
+    grade_badge1_ex16
+)
 
-from .badge_2 import grade_badge2_ex1
+from .badge_2 import (
+    grade_badge2_ex1
+)
