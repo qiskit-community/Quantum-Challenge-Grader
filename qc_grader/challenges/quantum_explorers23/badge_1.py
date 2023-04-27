62aed1abf9ce1a77400328225680b6b5b7bf89a8
quantum-kittens <14252649+quantum-kittens@users.noreply.github.com>
2023-04-27 19:32:15 +0530

    Add QE badge1 graders for ex 4-15 (#96)
    * Add grader functions for badge 1 questions 2 and 3
    
    * Add score question grader function
    
    * Update answer variables
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Add badge 1 ex16 grader
    
    * Add QE badge1 graders for ex 4-15
    
    ---------
    
    Co-authored-by: //va <vabarbosa@users.noreply.github.com>

--- qc_grader/challenges/quantum_explorers23/badge_1.py
+++ qc_grader/challenges/quantum_explorers23/badge_1.py
@@ -41,6 +41,129 @@ def grade_badge1_ex3(answer3: List[int]) -> None:
     )
     print(message)
 
+@typechecked
+def grade_badge1_ex4(answer4: List[int]) -> None:
+    status, _, message = grade(
+        answer4,
+        'badge1_ex4',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge1_ex5(answer5: List[int]) -> None:
+    status, _, message = grade(
+        answer5,
+        'badge1_ex5',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+
+@typechecked
+def grade_badge1_ex6(answer6: List[int]) -> None:
+    status, _, message = grade(
+        answer6,
+        'badge1_ex6',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+
+@typechecked
+def grade_badge1_ex7(answer7: List[int]) -> None:
+    status, _, message = grade(
+        answer7,
+        'badge1_ex7',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge1_ex8(answer8: List[int]) -> None:
+    status, _, message = grade(
+        answer8,
+        'badge1_ex8',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+
+@typechecked
+def grade_badge1_ex9(answer9: List[int]) -> None:
+    status, _, message = grade(
+        answer9,
+        'badge1_ex9',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge1_ex10(answer10: List[int]) -> None:
+    status, _, message = grade(
+        answer10,
+        'badge1_ex10',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge1_ex11(answer11: List[int]) -> None:
+    status, _, message = grade(
+        answer11,
+        'badge1_ex11',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge1_ex12(answer12: List[int]) -> None:
+    status, _, message = grade(
+        answer12,
+        'badge1_ex12',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge1_ex13(answer13: List[int]) -> None:
+    status, _, message = grade(
+        answer13,
+        'badge1_ex13',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge1_ex14(answer14: List[int]) -> None:
+    status, _, message = grade(
+        answer14,
+        'badge1_ex14',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge1_ex15(answer15: List[int]) -> None:
+    status, _, message = grade(
+        answer15,
+        'badge1_ex15',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
 
 @typechecked
 def grade_badge1_ex16(circuit: QuantumCircuit) -> None:
@@ -51,6 +174,8 @@ def grade_badge1_ex16(circuit: QuantumCircuit) -> None:
         return_response=True
     )
     print(message)
+
+
     
     
 @typechecked
