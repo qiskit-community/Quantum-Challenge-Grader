68b6be87dbcffd0ba2425c41efcc76dcc4dc7c6a
//va <vabarbosa@users.noreply.github.com>
2023-05-03 23:07:50 -0400

    add qml helpers (#98)

---
+++ qc_grader/qatutorial/module_2.py
@@ -0,0 +1,11 @@
+from typeguard import typechecked
+
+from qc_grader.grader.grade import grade
+
+
+_id = 'qml_tutorials'
+
+
+@typechecked
+def grade_module_2_ex1() -> None:
+    grade('', 'module-2', _id)
