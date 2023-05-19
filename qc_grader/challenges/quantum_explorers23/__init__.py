52b23f534299989ac76e08747b853210531f2511
quantum-kittens <14252649+quantum-kittens@users.noreply.github.com>
2023-05-19 17:02:48 +0530

    Add grader functions for QE badge5

--- qc_grader/challenges/quantum_explorers23/__init__.py
+++ qc_grader/challenges/quantum_explorers23/__init__.py
@@ -25,3 +25,24 @@ from .badge_2 import (
 from .badge_3 import (
     grade_badge3_ex1
 )
+
+
+from .badge_5 import (
+    grade_badge5_ex1,
+    grade_badge5_ex2,
+    grade_badge5_ex3,
+    grade_badge5_ex4,
+    grade_badge5_ex5,
+    grade_badge5_ex6,
+    grade_badge5_ex7,
+    grade_badge5_ex8,
+    grade_badge5_ex9,
+    grade_badge5_ex10,
+    grade_badge5_ex11,
+    grade_badge5_ex12,
+    grade_badge5_ex13,
+    grade_badge5_ex14,
+    grade_badge5_ex14,
+    grade_badge5_code,
+    grade_badge5_score
+)
