383a57b218ce1028f4c6111233951f0cfa265916
Kevin J. Sung <kevjsung@umich.edu>
2023-05-25 15:45:47 -0400

    add client code for qgss 2023 lab 4 (#108)

--- qc_grader/challenges/qgss_2023/__init__.py
+++ qc_grader/challenges/qgss_2023/__init__.py
@@ -7,5 +7,13 @@ from .lab1 import (
     grade_lab1_ex6,
     grade_lab1_ex7,
     grade_lab1_ex8,
-    grade_lab1_ex9
+    grade_lab1_ex9,
+)
+
+from .lab4 import (
+    grade_lab4_ex1,
+    grade_lab4_ex2,
+    grade_lab4_ex3,
+    grade_lab4_ex4,
+    grade_lab4_ex5,
 )
