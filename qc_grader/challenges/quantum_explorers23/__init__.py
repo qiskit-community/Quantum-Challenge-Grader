a5074373882fbd1391213e616327deb1083b96b9
quantum-kittens <14252649+quantum-kittens@users.noreply.github.com>
2023-06-07 20:44:00 +0530

    Add QE badge 4 graders and update init (#111)
    * Add grader functions for badge 1 questions 2 and 3
    
    * Add score question grader function
    
    * Update answer variables
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Update qc_grader/challenges/quantum_explorers23/badge_1.py
    
    * Add badge 1 ex16 grader
    
    * Add QE badge1 graders for ex 4-15
    
    * Update QE badge1 code question function
    
    * Add import for QE badge1 code question
    
    * Add import for QE badge1 code question
    
    * Add QE badge 3 grader
    
    * Add grader functions for QE badge5
    
    * Add graders for QE badge 5
    
    * Add graders for QE badge 5 q 14 and 15
    
    * Correct QE badge5 code function grader
    
    * Update variable  in QE Badge5 code question
    
    * Add QE badge4 graders and update init
    
    ---------
    
    Co-authored-by: //va <vabarbosa@users.noreply.github.com>
    Co-authored-by: va barbosa <va@us.ibm.com>

--- qc_grader/challenges/quantum_explorers23/__init__.py
+++ qc_grader/challenges/quantum_explorers23/__init__.py
@@ -26,6 +26,25 @@ from .badge_3 import (
     grade_badge3_ex1
 )
 
+from .badge_4 import (
+    grade_badge4_ex1,
+    grade_badge4_ex2,
+    grade_badge4_ex3,
+    grade_badge4_ex4,
+    grade_badge4_ex5,
+    grade_badge4_ex6,
+    grade_badge4_ex7,
+    grade_badge4_ex8,
+    grade_badge4_ex9,
+    grade_badge4_ex10,
+    grade_badge4_ex11,
+    grade_badge4_ex12,
+    grade_badge4_ex13,
+    grade_badge4_ex14,
+    grade_badge4_ex15,
+    grade_badge4_code,
+    grade_badge4_score
+)
 
 from .badge_5 import (
     grade_badge5_ex1,
---
+++ qc_grader/challenges/quantum_explorers23/badge_4.py
@@ -0,0 +1,205 @@
+from typing import List
+from typeguard import typechecked
+
+from qc_grader.grader.grade import grade
+
+import json
+
+from typeguard import typechecked
+from typing import Callable, List
+
+from qiskit.primitives import SamplerResult
+
+from qiskit_ibm_runtime.qiskit.primitives import (
+    SamplerResult as sampler_result
+)
+
+from qc_grader.grader.common import (
+    samplerresult_to_json,
+    probdistribution_to_json
+)
+
+
+_challenge_id = 'quantum_explorers23'
+
+
+@typechecked
+def grade_badge4_ex1(answer1: List[int]) -> None:
+    status, _, message = grade(
+        answer1,
+        'badge4_ex1',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+    
+    
+@typechecked
+def grade_badge4_ex2(answer2: List[int]) -> None:
+    status, _, message = grade(
+        answer2,
+        'badge4_ex2',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+    
+
+@typechecked
+def grade_badge4_ex3(answer3: List[int]) -> None:
+    status, _, message = grade(
+        answer3,
+        'badge4_ex3',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge4_ex4(answer4: List[int]) -> None:
+    status, _, message = grade(
+        answer4,
+        'badge4_ex4',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge4_ex5(answer5: List[int]) -> None:
+    status, _, message = grade(
+        answer5,
+        'badge4_ex5',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+
+@typechecked
+def grade_badge4_ex6(answer6: List[int]) -> None:
+    status, _, message = grade(
+        answer6,
+        'badge4_ex6',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+
+@typechecked
+def grade_badge4_ex7(answer7: List[int]) -> None:
+    status, _, message = grade(
+        answer7,
+        'badge4_ex7',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge4_ex8(answer8: List[int]) -> None:
+    status, _, message = grade(
+        answer8,
+        'badge4_ex8',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+
+@typechecked
+def grade_badge4_ex9(answer9: List[int]) -> None:
+    status, _, message = grade(
+        answer9,
+        'badge4_ex9',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge4_ex10(answer10: List[int]) -> None:
+    status, _, message = grade(
+        answer10,
+        'badge4_ex10',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge4_ex11(answer11: List[int]) -> None:
+    status, _, message = grade(
+        answer11,
+        'badge4_ex11',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge4_ex12(answer12: List[int]) -> None:
+    status, _, message = grade(
+        answer12,
+        'badge4_ex12',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+@typechecked
+def grade_badge4_ex13(answer13: List[int]) -> None:
+    status, _, message = grade(
+        answer13,
+        'badge4_ex13',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+    
+@typechecked
+def grade_badge4_ex14(answer14: List[int]) -> None:
+    status, _, message = grade(
+        answer14,
+        'badge4_ex14',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+    
+@typechecked
+def grade_badge4_ex15(answer15: List[int]) -> None:
+    status, _, message = grade(
+        answer15,
+        'badge4_ex15',
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+
+@typechecked
+def grade_badge4_code(sampler_results: List, quasi_dists: List) -> None: 
+    answer = {
+        'sampler_results': [samplerresult_to_json(r) for r in sampler_results],
+        'quasi_dists': [probdistribution_to_json(d) for d in quasi_dists], 
+    }
+    status, _, message = grade(
+        answer, 
+        'badge4_code', 
+        _challenge_id, 
+        return_response=True
+    )
+    print(message)
+
+
+@typechecked
+def grade_badge4_score(lang: str) -> None:
+    status, _, message = grade(
+        lang,
+        'badge4_score',
+        _challenge_id,
+        return_response=True
+    )
+    print(message)
