90ae418505c3337f46677e358ed2ec25d34b72a5
Sanket Panda <pandasa123@gmail.com>
2023-08-17 09:50:20 -0400

    Update Problem 5 grader

--- qc_grader/challenges/algorithm_design/problem_5.py
+++ qc_grader/challenges/algorithm_design/problem_5.py
@@ -10,11 +10,11 @@ _challenge_id = 'algorithm_design'
 
 
 @typechecked
-def grade_problem_5a(cost_function: callable) -> None:
+def grade_problem_5a(cost_function: callable, k: int, ansatz_list: TwoLocal, weight_vector: list[float], hamiltonian: SparsePauliOp, estimator: Estimator) -> float:
     evaluation_points = (2*np.pi*np.random.rand(50, 8)).tolist()
     user_values = []
     for theta in evaluation_points:
-        user_value = cost_function(theta)
+        user_value = cost_function(theta, k, ansatz_list, weight_vector, hamiltonian, estimator)
         user_values.append(user_value)
     grade({'evaluation_points': evaluation_points, 'user_values': user_values}, 'problem_5a', _challenge_id)
 
