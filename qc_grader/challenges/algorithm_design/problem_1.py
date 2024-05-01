3fa851eedd3365a6fd853b6123321d1ef44ca214
//va <va@us.ibm.com>
2024-05-01 14:21:11 -0400

    lint

--- qc_grader/challenges/algorithm_design/problem_1.py
+++ qc_grader/challenges/algorithm_design/problem_1.py
@@ -4,6 +4,7 @@ from qc_grader.grader.grade import grade
 
 _challenge_id = 'algorithm_design'
 
+
 @typechecked
 def grade_problem_1(answer: list) -> None:
     grade(answer, 'problem_1', _challenge_id)
