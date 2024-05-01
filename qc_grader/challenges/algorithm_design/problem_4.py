7e93d70b9fdd193e94400b1723a6af43d520a327
//va <vabarbosa@users.noreply.github.com>
2024-05-01 15:31:22 -0400

    clean up (#139)
    * remove unused
    
    * lint

--- qc_grader/challenges/algorithm_design/problem_4.py
+++ qc_grader/challenges/algorithm_design/problem_4.py
@@ -4,6 +4,7 @@ from qc_grader.grader.grade import grade
 
 _challenge_id = 'algorithm_design'
 
+
 @typechecked
 def grade_problem_4(answer: list) -> None:
     grade(answer, 'problem_4', _challenge_id)
