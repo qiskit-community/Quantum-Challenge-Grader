34acd54cd9fe234408d711fa9685558cdfb51187
//va <va@us.ibm.com>
2024-05-10 23:23:28 -0400

    initial commit

---
+++ qc_grader/challenges/iqc_2024/__init__.py
@@ -0,0 +1,36 @@
+from .lab0 import (
+    grade_lab0_ex1,
+    grade_lab0_ex2,
+    grade_lab0_ex3
+)
+
+
+from .lab1 import (
+    grade_lab1_ex1,
+    grade_lab1_ex2,
+    grade_lab1_ex3
+)
+
+from .lab2 import (
+    grade_lab2_ex1,
+    grade_lab2_ex2,
+    grade_lab2_ex3
+)
+
+from .lab3 import (
+    grade_lab3_ex1,
+    grade_lab3_ex2,
+    grade_lab3_ex3
+)
+
+from .lab4 import (
+    grade_lab4_ex1,
+    grade_lab4_ex2,
+    grade_lab4_ex3
+)
+
+from .lab5 import (
+    grade_lab5_ex1,
+    grade_lab5_ex2,
+    grade_lab5_ex3
+)
