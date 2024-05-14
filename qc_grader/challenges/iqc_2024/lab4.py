39e6361bc119a478ad711e635a33a78b5eb88756
//va <va@us.ibm.com>
2024-05-14 18:32:23 -0400

    lab4

--- qc_grader/challenges/iqc_2024/lab4.py
+++ qc_grader/challenges/iqc_2024/lab4.py
@@ -1,8 +1,8 @@
-from typing import List
+import json
 from typeguard import typechecked
 
-
-from qiskit import QuantumCircuit
+from qiskit_experiments.framework import ExperimentData, ExperimentEncoder
+from qiskit.quantum_info import PTM
 
 from qc_grader.grader.grade import grade
 
@@ -11,15 +11,26 @@ _challenge_id = 'iqc_2024'
 
 
 @typechecked
-def grade_lab4_ex1(answer: List[int]) -> None:
-    grade(answer, 'lab4-ex1', _challenge_id)
+def grade_lab4_ex1(
+    backend,
+    exp_data: ExperimentData,
+    t1_ptm: PTM
+) -> None:
+    # In case it's still running...
+    exp_data.block_for_results()
+
+    if err := exp_data.errors():
+        print(f"It looks like your experiment raised an error: {err}")
+        return
+
+    json_exp_data = json.dumps(exp_data, cls=ExperimentEncoder)
+    grade({
+        'backend_name': backend.name,
+        'experiment_data': json_exp_data,
+        'ptm_data': t1_ptm.data,
+    }, 'lab4-ex1', _challenge_id)
 
 
 @typechecked
-def grade_lab4_ex2(answer: str) -> None:
-    grade(answer, 'lab4-ex2', _challenge_id)
-
-
-@typechecked
-def grade_lab4_ex3(circuit: QuantumCircuit) -> None:
-    grade(circuit, 'lab4-ex3', _challenge_id)
+def grade_lab4_ex2(max_feasible_circuit_depth: int) -> None:
+    grade(max_feasible_circuit_depth, 'lab4-ex2', _challenge_id)
