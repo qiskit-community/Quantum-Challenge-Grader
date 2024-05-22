f19cc9ea86ec91565dbb0838b888bd10e5e6c758
//va <va@us.ibm.com>
2024-05-22 09:54:29 -0400

    swap 3 and 2

--- qc_grader/challenges/iqc_2024/lab2.py
+++ qc_grader/challenges/iqc_2024/lab2.py
@@ -1,8 +1,9 @@
-from typing import List
 from typeguard import typechecked
 
-
-from qiskit import QuantumCircuit
+from qiskit import transpile
+from qiskit.circuit.random import random_circuit
+from qiskit.transpiler import PassManager
+from qiskit_ibm_runtime.fake_provider import FakeTorino
 
 from qc_grader.grader.grade import grade
 
@@ -11,15 +12,35 @@ _challenge_id = 'iqc_2024'
 
 
 @typechecked
-def grade_lab2_ex1(answer: List[int]) -> None:
+def grade_lab2_ex1(answer: dict) -> None:
     grade(answer, 'lab2-ex1', _challenge_id)
 
 
 @typechecked
-def grade_lab2_ex2(answer: str) -> None:
-    grade(answer, 'lab2-ex2', _challenge_id)
+def grade_lab2_ex2(func: callable) -> None:
+    num_qubits = 5
+    depth = 5
+    qc = random_circuit(num_qubits, depth,measure=True, seed=10000)
+    qc_tr = transpile(qc, backend=FakeTorino(), optimization_level=3, seed_transpiler=1000)
+
+    out1 = func(qc_tr, FakeTorino())
+
+    grade({
+        'input': qc_tr,
+        'output': out1
+    }, 'lab2-ex2', _challenge_id)
 
 
 @typechecked
-def grade_lab2_ex3(circuit: QuantumCircuit) -> None:
-    grade(circuit, 'lab2-ex3', _challenge_id)
+def grade_lab2_ex3(answer: list) -> None:
+    grade(answer, 'lab2-ex3', _challenge_id)
+
+
+@typechecked
+def grade_lab2_ex4(answer: list) -> None:
+    grade(answer, 'lab2-ex4', _challenge_id)
+
+
+@typechecked
+def grade_lab2_ex5(answer: PassManager) -> None:
+    grade(answer, 'lab2-ex5', _challenge_id, to_bytes=True)
