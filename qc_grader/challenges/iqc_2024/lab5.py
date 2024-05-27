da08c9235ca0ea5ffd707836e7a0b0aff846f650
Junye Huang <h.jun.ye@gmail.com>
2024-05-27 23:05:32 +0200

    Add iqc_2024 lab5-ex1 client (#141)

--- qc_grader/challenges/iqc_2024/lab5.py
+++ qc_grader/challenges/iqc_2024/lab5.py
@@ -1,25 +1,22 @@
-from typing import List
 from typeguard import typechecked
 
+from typing import Callable
 
 from qiskit import QuantumCircuit
 
-from qc_grader.grader.grade import grade
-
+from qc_grader.grader.grade import grade, get_problem_set
 
 _challenge_id = 'iqc_2024'
 
-
 @typechecked
-def grade_lab5_ex1(answer: List[int]) -> None:
-    grade(answer, 'lab5-ex1', _challenge_id)
-
-
-@typechecked
-def grade_lab5_ex2(answer: str) -> None:
-    grade(answer, 'lab5-ex2', _challenge_id)
-
-
-@typechecked
-def grade_lab5_ex3(circuit: QuantumCircuit) -> None:
-    grade(circuit, 'lab5-ex3', _challenge_id)
+def grade_lab5_ex1(
+   gates_cut: set,
+   swap_depth: int,
+   cut_depth: float
+) -> None:
+   answer = {
+     'gates_cut': list(gates_cut),
+     'swap_depth': swap_depth,
+     'cut_depth': cut_depth
+   }
+   grade(answer, 'lab5-ex1', _challenge_id) 
