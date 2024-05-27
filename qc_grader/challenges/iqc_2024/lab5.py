4b56e57c2a1f303ce63a3ec3f5c90a771294f693
Junye Huang <h.jun.ye@gmail.com>
2024-05-27 16:38:12 +0200

    add lab5-ex1 draft from Kifumi

--- qc_grader/challenges/iqc_2024/lab5.py
+++ qc_grader/challenges/iqc_2024/lab5.py
@@ -1,25 +1,27 @@
-from typing import List
+import pickle
+import jsonpickle
+
 from typeguard import typechecked
 
+from typing import Callable
 
 from qiskit import QuantumCircuit
 
-from qc_grader.grader.grade import grade
-
+from qc_grader.grader.grade import grade, get_problem_set
 
 _challenge_id = 'iqc_2024'
 
-
 @typechecked
-def grade_lab5_ex1(answer: List[int]) -> None:
-    grade(answer, 'lab5-ex1', _challenge_id)
-
-
-@typechecked
-def grade_lab5_ex2(answer: str) -> None:
-    grade(answer, 'lab5-ex2', _challenge_id)
-
-
-@typechecked
-def grade_lab5_ex3(circuit: QuantumCircuit) -> None:
-    grade(circuit, 'lab5-ex3', _challenge_id)
+def grade_lab5_ex1(
+   gates_cut: set,
+   swap_depth: int,
+   cut_depth: float
+) -> None:
+   answer_dict = {
+     'gates_cut': gates_cut,
+     'swap_depth': swap_depth,
+     'cut_depth': cut_depth
+   }
+   answer = pickle.dumps(answer_dict).decode('ISO-8859-1')
+   
+   grade(answer, 'lab5-ex1', _challenge_id) 
