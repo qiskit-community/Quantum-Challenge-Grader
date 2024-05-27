67d98b2b5ba64d3cfe697446f19d76ea1faa1cc5
Junye Huang <h.jun.ye@gmail.com>
2024-05-27 23:04:14 +0200

    Add iqc_2024 lab3 client (#140)

--- qc_grader/challenges/iqc_2024/__init__.py
+++ qc_grader/challenges/iqc_2024/__init__.py
@@ -25,7 +25,10 @@ from .lab2 import (
 from .lab3 import (
     grade_lab3_ex1,
     grade_lab3_ex2,
-    grade_lab3_ex3
+    grade_lab3_ex3,
+    grade_lab3_ex4,
+    grade_lab3_ex5,
+    grade_lab3_ex6,
 )
 
 from .lab4 import (
