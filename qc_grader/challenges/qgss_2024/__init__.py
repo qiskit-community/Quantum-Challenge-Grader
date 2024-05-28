b97c1b3c697f2060c2f257d1fcf4e3a4971c7e15
//va <va@us.ibm.com>
2024-05-28 03:06:59 -0400

    lab 1

--- qc_grader/challenges/qgss_2024/__init__.py
+++ qc_grader/challenges/qgss_2024/__init__.py
@@ -1,7 +1,12 @@
 from .lab1 import (
     grade_lab1_ex1,
     grade_lab1_ex2,
-    grade_lab1_ex3
+    grade_lab1_ex3,
+    grade_lab1_ex4,
+    grade_lab1_ex5,
+    grade_lab1_ex6,
+    grade_lab1_ex7,
+    grade_lab1_ex8
 )
 
 from .lab2 import (
--- qc_grader/challenges/qgss_2024/lab1.py
+++ qc_grader/challenges/qgss_2024/lab1.py
@@ -1,25 +1,164 @@
-from typing import List
+import json
+
+from typing import Callable
 from typeguard import typechecked
 
 
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit, transpile
+from qiskit.circuit import Gate
+from qiskit.circuit.library import QFT
+from qiskit.providers.fake_provider import GenericBackendV2
+from qiskit.transpiler import StagedPassManager, CouplingMap, AnalysisPass, TransformationPass
 
 from qc_grader.grader.grade import grade
+from .utils import get_qc_in
 
 
 _challenge_id = 'qgss_2024'
 
 
+# Lab 1, Exercise 1A
 @typechecked
-def grade_lab1_ex1(answer: List[int]) -> None:
-    grade(answer, 'lab1-ex1', _challenge_id)
+def grade_lab1_ex1(qc_char_func: Callable) -> None:
+    if not callable(qc_char_func):
+        print("Please supply the function that determines circuit characteristics as specified")
+        return
+    
+    qc = QFT(5)
+    qc = transpile(qc, GenericBackendV2(5), seed_transpiler=307)
+    qc_info = qc_char_func(qc)
+
+    if not isinstance(qc_info, dict):
+        print("The circuit characteristics should be returned in the form of a dictionary as specified.")
+        return
+
+    grade({
+        'qc': qc,
+        'qc_info': qc_info
+    }, 'lab1-ex1', _challenge_id)
 
 
+# Lab 1, Exercise 1B
 @typechecked
-def grade_lab1_ex2(answer: str) -> None:
-    grade(answer, 'lab1-ex2', _challenge_id)
+def grade_lab1_ex2(qc_routed: QuantumCircuit) -> None:
+    if not isinstance(qc_routed, QuantumCircuit):
+        print("Input should be one single quantum circuit! Given:", type(qc_routed))
+        return
+
+    grade(qc_routed, 'lab1-ex2', _challenge_id)
 
 
+# Lab 1, Exercise 2A
 @typechecked
-def grade_lab1_ex3(circuit: QuantumCircuit) -> None:
-    grade(circuit, 'lab1-ex3', _challenge_id)
+def grade_lab1_ex3(pm_staged: StagedPassManager) -> None:
+    if not isinstance(pm_staged, StagedPassManager):
+        print("Input should a staged pass manager! Given:", type(pm_staged))
+        return
+
+    num_qubits = 10
+    qc = QFT(num_qubits, do_swaps=False)
+    qc_out = pm_staged.run(qc)
+
+    grade({
+        'qc': qc,
+        'qc_out': qc_out
+    }, 'lab1-ex3', _challenge_id)
+
+
+# Lab 1, Exercise 2B
+@typechecked
+def grade_lab1_ex4(pm_staged: StagedPassManager) -> None:
+    if not isinstance(pm_staged, StagedPassManager):
+        print("Input should a staged pass manager! Given:", type(pm_staged))
+        return
+
+    num_qubits = 10
+    qc = QFT(num_qubits, do_swaps=False)
+    qc_out = pm_staged.run(qc)
+
+    grade({
+        'qc': qc,
+        'qc_out': qc_out
+    }, 'lab1-ex4', _challenge_id)
+
+
+# Lab 1, Exercise 2C
+@typechecked
+def grade_lab1_ex5(pm_staged: StagedPassManager) -> None:
+    if not isinstance(pm_staged, StagedPassManager):
+        print("Input should a staged pass manager! Given:", type(pm_staged))
+        return
+
+    num_qubits = 10
+    qc = QFT(num_qubits, do_swaps=False)
+    qc_out = pm_staged.run(qc)
+    qc_qk = transpile(qc, backend=GenericBackendV2(num_qubits, coupling_map=CouplingMap.from_line(num_qubits)), seed_transpiler=307)
+    opt_cx_count = qc_out.num_nonlocal_gates()
+    qk_cx_count = qc_qk.num_nonlocal_gates()
+
+    grade({
+        'qc': qc,
+        'qc_out': qc_out,
+        'opt_cx_count': opt_cx_count,
+        'qk_cx_count': qk_cx_count
+    }, 'lab1-ex5', _challenge_id)
+
+
+# Lab 1, Exercise 3A
+@typechecked
+def grade_lab1_ex6(user_pass_class: AnalysisPass) -> None:
+    if not issubclass(user_pass_class, AnalysisPass) and not isinstance(user_pass_class, AnalysisPass):
+        print("The supplied pass is not an AnalysisPass!")
+        return
+
+    num_qubits = 5
+    qc = transpile(QFT(num_qubits, do_swaps=False), backend=GenericBackendV2(num_qubits, coupling_map=CouplingMap.from_line(num_qubits)), seed_transpiler=307)
+    user_pass = user_pass_class()
+    user_pass(qc)
+
+    grade({
+        'userpass': [user_pass.property_set["one_q_op"], user_pass.property_set["two_q_op"]],
+        'qc': qc
+    }, 'lab1-ex6', _challenge_id)
+
+
+# Lab 1, Exercise 3B
+@typechecked
+def grade_lab1_ex7(user_pass_class: AnalysisPass, pg: Gate) -> None:
+    if not issubclass(user_pass_class, TransformationPass) and not isinstance(user_pass_class, TransformationPass):
+        print("The supplied pass is not a TransformationPass!")
+        return
+
+    nq = 5
+    qc = get_qc_in(nq, pg)
+    user_pass = user_pass_class()
+    qc_user = user_pass(qc)
+
+    grade({
+        'qc': qc,
+        'qc_user': qc_user
+    }, 'lab1-ex7', _challenge_id)
+
+
+# Lab 1, Exercise 3C
+@typechecked
+def grade_lab1_ex8(user_pass: AnalysisPass, pg: Gate) -> None:
+    if not issubclass(user_pass, TransformationPass) and not isinstance(user_pass, TransformationPass):
+        print("The supplied pass is not a TransformationPass!")
+        return
+    
+    nq = 5
+    qc = get_qc_in(nq)
+    qc_user = user_pass(qc)
+
+    correct, _, characteristics = grade({
+        'qc': qc,
+        'qc_user': qc_user
+    }, 'lab1-ex8', _challenge_id, return_response=True)
+
+    if not correct:
+        print(f'\nOops 😕! {"Your answer is incorrect" if characteristics is None else characteristics}')
+    else:
+        print('\nCongratulations 🎉! Your answer is correct.')
+        print(f'characteristics:', json.loads(characteristics))
+    return characteristics
