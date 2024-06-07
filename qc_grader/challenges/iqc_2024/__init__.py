e90da55e9194d400176f25a0957d8ffbe30889f7
Vishal Bajpe <Vishal.Bajpe@ibm.com>
2024-06-07 14:22:20 +0800

    feat: add graders

--- qc_grader/challenges/iqc_2024/__init__.py
+++ qc_grader/challenges/iqc_2024/__init__.py
@@ -20,13 +20,8 @@ from .lab2 import (
 )
 
 from .lab3 import (
-    grade_lab3_ckt_ex1,
-    grade_lab3_ckt_ex2,
-    grade_lab3_qs_ex1,
     grade_lab3_ait_ex1,
     grade_lab3_ait_ex2,
-    # grade_lab3_qca_ex1,
-    # grade_lab3_qca_ex2
 )
 
 from .lab4 import grade_lab4_ex1, grade_lab4_ex2, grade_lab4_ex3
