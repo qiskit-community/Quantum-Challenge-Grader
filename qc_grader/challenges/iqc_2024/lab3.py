730b3e7d58ddcd3366fafe85b1d47fd89f3ce1e4
Vishal Bajpe <Vishal.Bajpe@ibm.com>
2024-06-10 10:18:15 +0800

    feat: add serverless graders

--- qc_grader/challenges/iqc_2024/lab3.py
+++ qc_grader/challenges/iqc_2024/lab3.py
@@ -3,7 +3,10 @@ from typeguard import typechecked
 from typing import Callable
 
 from qiskit import QuantumCircuit
+from qiskit_serverless.core.job import Job
 from qiskit.quantum_info import SparsePauliOp
+from qiskit_serverless.core.function import QiskitFunction
+
 
 from qc_grader.grader.grade import grade, get_problem_set
 from qiskit_transpiler_service.transpiler_service import TranspilerService
@@ -65,3 +68,54 @@ def grade_lab3_ckt_ex2(
         "cut_depth": isa_qpd_toffoli_depth_2_mean,
     }
     grade(answer, "lab3-ckt-ex2", _challenge_id)
+
+
+@typechecked
+def grade_lab3_qs_ex1(
+    function: QiskitFunction, input_arguments: dict, job: Job
+) -> None:
+    check_entry = function.entrypoint
+    job_valid = isinstance(job, Job)
+    circuit_valid = isinstance(input_arguments["ansatz"], QuantumCircuit)
+    op_valid = isinstance(input_arguments["operator"], SparsePauliOp)
+    answer = [
+        check_entry,
+        input_arguments["method"],
+        op_valid,
+        circuit_valid,
+        job_valid,
+    ]
+    grade(answer, "lab3-qs-ex1", _challenge_id)
+
+
+@typechecked
+def grade_lab3_qs_ex2(
+    optimization_levels: list,
+    transpiler_services: list,
+    transpile_parallel_circuit: QiskitFunction,
+    transpile_parallel_serverless: QiskitFunction,
+    job: Job,
+) -> None:
+    check_entry = transpile_parallel_circuit.entrypoint
+    job_valid = isinstance(job, Job)
+    transpilerservice_check = [
+        [
+            service["service"].ai,
+            service["service"].backend_name,
+            service["service"].optimization_level,
+        ]
+        for service in transpiler_services
+    ]
+    config_check = [
+        transpile_parallel_serverless.raw_data["title"],
+        transpile_parallel_serverless.raw_data["entrypoint"],
+    ]
+
+    answer = [
+        list(set(optimization_levels)),
+        transpilerservice_check,
+        check_entry,
+        config_check,
+        job_valid,
+    ]
+    grade(answer, "lab3-qs-ex2", _challenge_id)
