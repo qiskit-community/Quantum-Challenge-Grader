bba9d22b9d3a29abffc619dd9ab58b04d6bf8170
Alejandra Perea Rojas <aleperea@ibm.com>
2024-10-30 14:48:20 -0400

    Remove Day 0, add day 1

--- qc_grader/challenges/qdc_2024/__init__.py
+++ qc_grader/challenges/qdc_2024/__init__.py
@@ -1,1 +1,9 @@
-from .day0 import grade_day0_ex1
+from .day1 import (
+    day1_score_func,
+    grade_day1_ex1,
+    grade_day1_ex2,
+    grade_day1_ex3,
+    grade_day1_ex4,
+    grade_day1_ex5,
+    grade_day1_ex6,
+)
--- qc_grader/challenges/qdc_2024/day1.py
+++ qc_grader/challenges/qdc_2024/day1.py
@@ -4,3 +4,22 @@ from qc_grader.grader.grade import grade
 
 _challenge_id = 'qdc_2024'
 
+@typechecked
+def grade_day1_ex1(answer: str) -> None:
+    grade(
+        answer=answer, 
+        question='day1-ex1',
+        challenge=_challenge_id
+    )
+
+def grade_day1_ex2(answer) -> None: ...
+
+def grade_day1_ex3(answer) -> None: ...
+
+def grade_day1_ex4(answer) -> None: ...
+
+def grade_day1_ex5(answer) -> None: ...
+
+def grade_day1_ex6(answer) -> None: ...
+
+def day1_score_func(answer) -> None: ...
