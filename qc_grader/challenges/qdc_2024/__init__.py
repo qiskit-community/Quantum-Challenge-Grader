15ad7334afcfa5efdc3d4907a096b26a4a7197c8
Alejandra Perea Rojas <aleperea@ibm.com>
2024-11-08 15:56:27 -0500

    Narrow down changes to setup

--- qc_grader/challenges/qdc_2024/__init__.py
+++ qc_grader/challenges/qdc_2024/__init__.py
@@ -1,9 +1,1 @@
-from .day1 import (
-    day1_score_func,
-    grade_day1_ex1,
-    grade_day1_ex2,
-    grade_day1_ex3,
-    grade_day1_ex4,
-    grade_day1_ex5,
-    grade_day1_ex6,
-)
+from .utils import submit_name, submit_feedback, test_submit
--- qc_grader/challenges/qdc_2024/day3.py
+++
@@ -1,6 +0,0 @@
-from typeguard import typechecked
-
-from qc_grader.grader.grade import grade
-
-_challenge_id = 'qdc_2024'
-
