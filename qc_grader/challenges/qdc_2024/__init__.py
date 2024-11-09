d115107924a58828384bb185e68cdc4cb2897252
Junye Huang <h.jun.ye@gmail.com>
2024-11-09 01:27:27 +0100

    add day2 grader

--- qc_grader/challenges/qdc_2024/__init__.py
+++ qc_grader/challenges/qdc_2024/__init__.py
@@ -1,1 +1,10 @@
 from .day1 import grade_day1a_ex1, grade_day1a_ex2, grade_day1b_ex1
+from .day2 import (
+    grade_day2a_ex1,
+    grade_day2a_ex2,
+    grade_day2a_ex3,
+    grade_day2a_ex4,
+    grade_day2a_ex5,
+    grade_day2b_ex1,
+)
+from .utils import build_max_cut_paulis
