77f40edbba0ff034c7833e296cabd2b807bc38ea
Alejandra Perea Rojas <aleperea@ibm.com>
2024-11-12 10:30:34 -0500

    Set up QDC2024

---
+++ qc_grader/challenges/qdc_2024/utils.py
@@ -0,0 +1,18 @@
+from typeguard import typechecked
+
+from qc_grader.grader.grade import grade
+
+
+_challenge_id = 'qdc_2024'
+
+@typechecked
+def submit_name(name: str) -> None:
+    grade(name, 'submit-name', _challenge_id)
+
+@typechecked
+def submit_feedback(feedback: str) -> None:
+    grade(feedback, 'submit-feedback', _challenge_id)
+
+@typechecked
+def test_submit(answer: str) -> None:
+    grade(answer, 'test', _challenge_id)
