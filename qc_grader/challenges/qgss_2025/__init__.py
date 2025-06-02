d0367e84e271087718c70afcd56dc8c86209e2ae
Marcel Pfaffhauser <faf@zurich.ibm.com>
2025-06-02 11:53:43 +0200

    Initial draft

---
+++ qc_grader/challenges/qgss_2025/__init__.py
@@ -0,0 +1,35 @@
+from .lab1 import (
+    grade_lab1_ex1,
+    grade_lab1_ex2,
+    grade_lab1_ex3,
+    grade_lab1_ex4,
+    grade_lab1_ex5,
+    grade_lab1_ex6,
+    grade_lab1_ex7,
+    grade_lab1_ex8
+)
+
+from .lab2 import (
+    grade_lab2_ex1,
+    grade_lab2_ex2,
+    grade_lab2_ex3,
+    grade_lab2_ex4
+)
+
+from .lab3 import (
+    grade_lab3_ex1,
+    grade_lab3_ex2,
+    grade_lab3_ex3
+)
+
+from .lab4 import (
+    grade_lab4_ex1,
+    grade_lab4_ex2,
+    grade_lab4_ex3,
+    grade_lab4_ex4,
+    grade_lab4_ex5,
+    grade_lab4_ex6,
+    grade_lab4_ex7,
+    lab4_ex7_get_data,
+    grade_lab4_ex8
+)
