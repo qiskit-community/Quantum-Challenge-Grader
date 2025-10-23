8bd7622981da363b7fdf09a2c2a0a441f8f16e11
Tonia Elengikal <Tonia.Elengikal@ibm.com>
2025-10-23 14:10:06 -0400

    fixing import errors and syntax errors for qdc 2025 lab 1

--- qc_grader/challenges/qdc_2025/__init__.py
+++ qc_grader/challenges/qdc_2025/__init__.py
@@ -1,7 +1,3 @@
-from .utils import build_max_cut_paulis
-from .utils import submit_name, submit_feedback, test_submit
-
-
 from .qdc25_lab1 import (
     grade_lab1_ex1,
     grade_lab1_ex2,
@@ -9,4 +5,3 @@ from .qdc25_lab1 import (
     grade_lab1_ex4,
     grade_lab1_ex5
 )
-
