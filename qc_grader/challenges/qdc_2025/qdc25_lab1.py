12db52487b078c2958ccb68e94dba5fb78ca58d3
Vishal Bajpe <Vishal.Bajpe@ibm.com>
2025-10-25 00:47:13 +0800

    chore: black

--- qc_grader/challenges/qdc_2025/qdc25_lab1.py
+++ qc_grader/challenges/qdc_2025/qdc25_lab1.py
@@ -7,46 +7,45 @@ from qiskit_ibm_runtime.ibm_backend import IBMBackend
 import numpy
 
 
-_challenge_id = 'qdc_2025'
+_challenge_id = "qdc_2025"
+
 
 @typechecked
 def submit_name(name: str) -> None:
-    status, score, message = grade(name, 'submit-name', _challenge_id, return_response=True)
+    status, score, message = grade(
+        name, "submit-name", _challenge_id, return_response=True
+    )
     if status == False:
         print(message)
     else:
-        print('Team name submitted.')
+        print("Team name submitted.")
+
 
 @typechecked
 def grade_lab1_ex1(qc: QuantumCircuit) -> None:
-    grade(qc, 'lab1-ex1', _challenge_id)
+    grade(qc, "lab1-ex1", _challenge_id)
 
 
 @typechecked
 def grade_lab1_ex2(qc: QuantumCircuit, lattice: HeavyHexLattice) -> None:
     plaq_width = lattice.plaquettes_width
     plaq_height = lattice.plaquettes_height
-    answer_dict = {
-        "plaq_width": plaq_width,
-        "plaq_height": plaq_height,
-        "qc": qc
-    }
-    grade(answer_dict, 'lab1-ex2', _challenge_id)
+    answer_dict = {"plaq_width": plaq_width, "plaq_height": plaq_height, "qc": qc}
+    grade(answer_dict, "lab1-ex2", _challenge_id)
+
 
 @typechecked
 def grade_lab1_ex3(qc: QuantumCircuit, lattice: HeavyHexLattice) -> None:
     plaq_width = lattice.plaquettes_width
     plaq_height = lattice.plaquettes_height
-    answer_dict = {
-        "plaq_width": plaq_width,
-        "plaq_height": plaq_height,
-        "qc": qc
-    }
-    grade(answer_dict, 'lab1-ex3', _challenge_id)
+    answer_dict = {"plaq_width": plaq_width, "plaq_height": plaq_height, "qc": qc}
+    grade(answer_dict, "lab1-ex3", _challenge_id)
 
 
 @typechecked
-def grade_lab1_ex4(isa_circuits: list, lattice: HeavyHexLattice, dt: float, backend: IBMBackend) -> None:
+def grade_lab1_ex4(
+    isa_circuits: list, lattice: HeavyHexLattice, dt: float, backend: IBMBackend
+) -> None:
     plaq_width = lattice.plaquettes_width
     plaq_height = lattice.plaquettes_height
 
@@ -54,16 +53,22 @@ def grade_lab1_ex4(isa_circuits: list, lattice: HeavyHexLattice, dt: float, back
         "isa_circuits": isa_circuits,
         "plaq_width": plaq_width,
         "plaq_height": plaq_height,
-        "dt": dt
+        "dt": dt,
     }
-    grade(answer_dict, 'lab1-ex4', _challenge_id)
+    grade(answer_dict, "lab1-ex4", _challenge_id)
+
 
 @typechecked
-def grade_lab1_ex5(best_expectation_vals: numpy.ndarray, qubit: int, dt: float, classical_exp_vals: numpy.ndarray ) -> None:
+def grade_lab1_ex5(
+    best_expectation_vals: numpy.ndarray,
+    qubit: int,
+    dt: float,
+    classical_exp_vals: numpy.ndarray,
+) -> None:
     answer_dict = {
         "best_expectation_vals": best_expectation_vals,
         "qubit": qubit,
         "dt": dt,
-        "classical_exp_vals": classical_exp_vals
+        "classical_exp_vals": classical_exp_vals,
     }
-    grade(answer_dict, 'lab1-ex5', _challenge_id)
+    grade(answer_dict, "lab1-ex5", _challenge_id)
