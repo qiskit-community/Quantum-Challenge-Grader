ae9fccb46c4ea39b29a0a9dfd27cab93ca2f3b02
Vishal Bajpe <Vishal.Bajpe@ibm.com>
2025-10-25 00:47:03 +0800

    feat: add imports

--- qc_grader/challenges/qdc_2025/__init__.py
+++ qc_grader/challenges/qdc_2025/__init__.py
@@ -4,5 +4,21 @@ from .qdc25_lab1 import (
     grade_lab1_ex2,
     grade_lab1_ex3,
     grade_lab1_ex4,
-    grade_lab1_ex5
+    grade_lab1_ex5,
+)
+
+from .qdc25_lab2 import (
+    submit_name,
+    grade_lab2_ex1,
+    grade_lab2_ex2,
+    grade_lab2_ex3,
+    grade_lab2_ex4,
+    grade_lab2_ex5,
+    grade_lab2_ex6,
+    grade_lab2_ex7,
+    grade_lab2_ex8,
+    grade_lab2_ex9,
+    grade_lab2_ex10,
+    grade_lab2_ex11,
+    grade_lab2_ex12,
 )
