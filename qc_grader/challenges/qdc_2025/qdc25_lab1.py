da169f4f91fc4651915955eea4b47e9fb8d459bf
Joana Fraxanet <45035009+jfraxanet@users.noreply.github.com>
2025-10-31 19:12:03 +0100

    Update qdc25_lab1.py

--- qc_grader/challenges/qdc_2025/qdc25_lab1.py
+++ qc_grader/challenges/qdc_2025/qdc25_lab1.py
@@ -59,7 +59,7 @@ def grade_lab1_ex3(qc: QuantumCircuit, lattice: HeavyHexLike) -> None:
 
 
 def grade_lab1_ex4(
-    isa_circuits: list, lattice: HeavyHexLike, dt: float, backend: IBMBackend
+    isa_circuits: QuantumCircuit, lattice: HeavyHexLike, dt: float, backend: IBMBackend
 ) -> None:
     check_type(isa_circuits, QuantumCircuit)
     check_type(lattice, HeavyHexLike)
