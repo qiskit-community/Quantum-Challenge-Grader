523a5997d4be2fd7f5db521d6b7e4e262b61e20e
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2026-04-15 10:18:50 -0400

    Restore submit_name to QDC 2025 (#286)
    We got a user report this morning that this import failed:
    
    ```
    AttributeError: module 'qc_grader.challenges.qdc_2025.qdc25_lab6' has no attribute 'submit_name'"
    ```
    
    https://github.com/qiskit-community/Quantum-Challenge-Grader/pull/279
    broke them because we removed the duplication functions, with the idea
    that people would import `qc_grader.challenges.qdc_2025`. Turns out,
    that was wrong. We add back that code.

--- qc_grader/challenges/qdc_2025/qdc25_lab6.py
+++ qc_grader/challenges/qdc_2025/qdc25_lab6.py
@@ -16,6 +16,17 @@ _challenge_id = "qdc_2025"
 
 
 @typechecked
+def submit_name(name: str) -> None:
+    status, score, message = grade(
+        name, "submit-name", _challenge_id, return_response=True
+    )
+    if status is False:
+        print(message)
+    else:
+        print("Team name submitted.")
+
+
+@typechecked
 def grade_lab6_ex1(molecule_name: str, hartree_fock_E: float) -> None:
 
     answer_dict = {
