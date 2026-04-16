53881051ed5b6dac53553bd01ec49cbb76edf547
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2026-04-16 17:21:30 -0400

    Nits

--- qc_grader/challenges/qgss_2026/lab0.py
+++ qc_grader/challenges/qgss_2026/lab0.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 # (C) Copyright IBM 2026
 #
 # This code is licensed under the Apache License, Version 2.0. You may
@@ -15,36 +12,36 @@
 QGSS 2026 Lab 0 - Grading Functions
 """
 
-from typing import List
+from qiskit_ibm_runtime.ibm_backend import IBMBackend
 
 from typeguard import typechecked
 from qiskit.quantum_info import Statevector
 from qc_grader.grader.grade import grade
 
 
-_challenge_id = "qgss_2026"
+_CHALLENGE_ID = "qgss_2026"
 
 
 @typechecked
-def grade_lab0_ex1(backends: list) -> None:
+def grade_lab0_ex1(backends: list[IBMBackend]) -> None:
     """
     Grade Exercise 1: Verify IBM Quantum account.
 
     Args:
         backends: List of backend objects from service.backends()
     """
-    grade(len(backends), "lab0-ex1", _challenge_id)
+    grade(len(backends), "lab0-ex1", _CHALLENGE_ID)
 
 
 @typechecked
-def grade_lab0_ex2(counts: dict) -> None:
+def grade_lab0_ex2(counts: dict[str, int]) -> None:
     """
     Grade Exercise 2: Verify sampler result.
 
     Args:
         counts: Dictionary of measurement counts
     """
-    grade(counts, "lab0-ex2", _challenge_id)
+    grade(counts, "lab0-ex2", _CHALLENGE_ID)
 
 
 @typechecked
@@ -55,11 +52,11 @@ def grade_lab0_ex3(exp_val: float) -> None:
     Args:
         exp_val: Expectation value from Estimator
     """
-    grade(exp_val, "lab0-ex3", _challenge_id)
+    grade(exp_val, "lab0-ex3", _CHALLENGE_ID)
 
 
 @typechecked
-def grade_lab0_ex4(c_out: List[str], cpp_out: List[str]) -> None:
+def grade_lab0_ex4(c_out: list[str], cpp_out: list[str]) -> None:
     """
     Grade Exercise 4: Verify C/C++ circuit outputs.
 
@@ -71,7 +68,7 @@ def grade_lab0_ex4(c_out: List[str], cpp_out: List[str]) -> None:
         "c_out": " ".join(str(x) for x in c_out),
         "cpp_out": " ".join(str(x) for x in cpp_out),
     }
-    grade(answer_dict, "lab0-ex4", _challenge_id)
+    grade(answer_dict, "lab0-ex4", _CHALLENGE_ID)
 
 
 @typechecked
@@ -82,4 +79,4 @@ def grade_lab0_ex5(sv: Statevector) -> None:
     Args:
         sv: Statevector object to verify
     """
-    grade(sv, "lab0-ex5", _challenge_id)
+    grade(sv, "lab0-ex5", _CHALLENGE_ID)
