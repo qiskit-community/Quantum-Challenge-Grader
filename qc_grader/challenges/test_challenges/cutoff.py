561b4f0c6b0a92059823efb22c88b0eaea80437e
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2026-04-30 17:43:43 -0400

    Support multiple test challenges

---
+++ qc_grader/challenges/test_challenges/cutoff.py
@@ -0,0 +1,26 @@
+# (C) Copyright IBM 2024
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+from typeguard import typechecked
+
+from qc_grader.grader.grade import grade
+
+
+_CHALLENGE_ID = "test_cutoff"
+
+
+@typechecked
+def grade_success(answer: str) -> None:
+    grade(answer, "test-pass", _CHALLENGE_ID)
+
+
+@typechecked
+def grade_fail(answer: str) -> None:
+    grade(answer, "test-fail", _CHALLENGE_ID)
