e9ac3e99ab958e17cd9442637dd297cdcdcfb99a
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2026-05-08 15:02:01 -0400

    Use new API for submissions

--- qc_grader/grader/api.py
+++ qc_grader/grader/api.py
@@ -8,9 +8,9 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-import requests
+from typing import Any
 
-from typing import Mapping
+import requests
 
 from qc_grader import __version__
 from qc_grader.grader.auth import get_access_token
@@ -19,39 +19,24 @@ from qc_grader.grader.env import GRADER_BASE_URL
 
 def send_request(
     endpoint: str,
-    query: dict[str, str] | None = None,
-    body: dict[str, str] | None = None,
+    body: dict[str, Any] | None = None,
     method: str = "POST",
-    header: Mapping[str, str] | None = None,
-) -> dict[str, str]:
+) -> dict[str, Any]:
     headers = {
         "Accept": "application/json",
         "Content-Type": "application/json",
         "X-Client-Version": __version__,
         "Authorization": f"Bearer {get_access_token()}",
-        **(header or {}),
     }
 
     response = requests.request(
-        method,
-        url=f"{GRADER_BASE_URL}{endpoint}",
-        params=query,
-        json=body,
-        headers=headers,
+        method, url=f"{GRADER_BASE_URL}{endpoint}", json=body, headers=headers
     )
 
     if response.status_code == 200:
         return response.json()
 
-    if response.status_code == 403:
-        raise Exception(f"Unable to access service ({response.reason})")
+    if response.status_code == 204:
+        return {}
 
-    try:
-        result = response.json()
-        if "error" in result:
-            result = result["error"]
-        if "message" in result:
-            result = result["message"]
-    except Exception:
-        result = f" Not successful - {response.reason}"
-    raise Exception(result)
+    raise Exception(response.text)
