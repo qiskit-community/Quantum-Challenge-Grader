b548de01538b986607de5461c885d34fdf8dcad4
Gwonhak Lee <gwonhak.lee@ibm.com>
2026-05-28 17:25:27 +0900

    completed exercise 1-4

--- qc_grader/challenges/qgss_2026/lab2.py
+++ qc_grader/challenges/qgss_2026/lab2.py
@@ -12,11 +12,16 @@
 QGSS 2026 Lab 2 - Grading Functions
 """
 
-from typing import Any
-
+from typing import Any, Callable, TypedDict
 from typeguard import typechecked
+import base64
+from io import BytesIO
+import numpy as np
 
 from qc_grader.grader.grade import grade_answer
+
+from qiskit import QuantumCircuit, qpy
+from qiskit_aer import AerSimulator
 
 _CHALLENGE = "qgss_2026"
 _LAB = "lab2"
@@ -36,3 +41,62 @@ def grade_lab2_ex1(basis_operations: list[str],
     answer_dict = {"basis_operations": basis_operations,
                    "coupling_map": [(q1, q2) for q1, q2 in coupling_map]}
     _grade(answer_dict, "ex1")
+
+
+def circuit_to_qpy_base64(circuit: QuantumCircuit) -> str:
+    buffer = BytesIO()
+    qpy.dump(circuit, buffer)
+    return base64.b64encode(buffer.getvalue()).decode("ascii")
+
+def simulator_to_dict(simulator: AerSimulator) -> dict:
+    return {
+        "noise_model": simulator.options.noise_model.to_dict(serializable=True)
+    }
+
+@typechecked
+def grade_lab2_ex2(repeated_x_circuit: Callable[[int], QuantumCircuit],
+                   depol_x_simulator: Callable[[float], AerSimulator]) -> None:
+    """
+    Grade Exercise 2: Check the repeated X circuit and depolarized simulator.
+    """
+    test_ns = [0, 1, 2, 3, 5, 10]
+    test_lambdas = [0.0, 0.02, 0.04, 0.1, 0.5, 1.0]    
+    answer_dict = {
+        "repeated_x_circuit": {
+            n: circuit_to_qpy_base64(repeated_x_circuit(n)) for n in test_ns
+        },
+        "depol_x_simulator": {
+            lam: simulator_to_dict(depol_x_simulator(lam)) for lam in test_lambdas
+        }
+    }
+    _grade(answer_dict, "ex2")
+
+Ex3InputCase = TypedDict("Ex3InputCase", {
+    "true lambda": np.float64 | float,
+    "fitted lambda": np.float64 | float,
+    "fit std": np.float64 | float
+})
+
+@typechecked
+def grade_lab2_ex3(lambda_fitting: list[Ex3InputCase]) -> None:
+    """
+    Grade Exercise 3: Check the estimated lambda vs true lambda
+    """
+    answer_dict = {
+        "lambda_fitting_cases": lambda_fitting
+    }
+    _grade(answer_dict, "ex3")
+
+
+@typechecked
+def grade_lab2_ex4(repeated_x_meas_x_circuit: Callable[[int], QuantumCircuit]) -> None:
+    """
+    Grade Exercise 4: Check the repeated X circuit on X basis.
+    """
+    test_ns = [0, 1, 2, 3, 5, 10]
+    answer_dict = {
+        "repeated_x_meas_x_circuit": {
+            n: circuit_to_qpy_base64(repeated_x_meas_x_circuit(n)) for n in test_ns
+        }
+    }
+    _grade(answer_dict, "ex4")
