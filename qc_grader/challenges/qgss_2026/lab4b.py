ad64cb2bc7e2016b6320f77d84aae7ca3bee0c96
Jorge Martinez <Jorge.Martinez@ibm.com>
2026-05-29 10:18:41 +0200

    suggestions from code review

--- qc_grader/challenges/qgss_2026/lab4b.py
+++ qc_grader/challenges/qgss_2026/lab4b.py
@@ -57,7 +57,7 @@ def grade_lab4b_ex1b(
 
 @typechecked
 def grade_lab4b_ex2(
-    options_list: list[SamplerOptions | dict],
+    options_list: list[SamplerOptions],
     counts_list: list[dict[str, int]],
     m3_quasis_v3: dict[str, float | np.floating],
     m3_quasis_v4: dict[str, float | np.floating],
