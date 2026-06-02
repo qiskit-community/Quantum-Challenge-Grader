e82df43e3a91dc38edf2838b5f145f1f8af12747
Sophie Engineer <85221888+sophieengineer@users.noreply.github.com>
2026-06-02 16:40:27 +0100

    convert PauliLindbladMap to sparse lists

--- qc_grader/challenges/qgss_2026/lab3.py
+++ qc_grader/challenges/qgss_2026/lab3.py
@@ -27,10 +27,24 @@ def _grade(answer: Any, exercise: str) -> None:
 
 
 @typechecked
-def grade_lab3_ex5(circuit_ising: QuantumCircuit, mirrored_circuit: QuantumCircuit, boxed: QuantumCircuit, obs_list: list[SparsePauliOp], forward_list: list[dict[str, PauliLindbladMap]], backward_list: list[dict[str, PauliLindbladMap]]) -> None:
+def grade_lab3_ex5(circuit_ising: QuantumCircuit, mirrored_circuit: QuantumCircuit, boxed: QuantumCircuit, obs_list: list[SparsePauliOp], forward_list: list[dict[str, PauliLindbladMap]], backward_bound: dict[str, PauliLindbladMap]) -> None:
     """
     Grade Exercise 5: Investigate the locality of 3 observables for 15 qubits
     """
-    answer_dict = {"circuit_ising": circuit_ising, "mirrored_circuit": mirrored_circuit, "boxed": boxed, "obs_list": obs_list, "forward_list": forward_list, "backward_list": backward_list}
+    # convert PauliLindbladMap to sparse lists
+    for key in backward_bound:
+        backward_bound[key] = backward_bound[key].to_sparse_list()
+
+    for i, d in enumerate(forward_list):
+        for key, pl_map in d.items():
+            forward_list[i][key] = pl_map.to_sparse_list()
+    
+    answer_dict = {
+        "circuit_ising": circuit_ising, 
+        "mirrored_circuit": mirrored_circuit, 
+        "boxed": boxed, "obs_list": obs_list, 
+        "forward_list": forward_list, 
+        "backward_bound": backward_bound}
+    
     _grade(answer_dict, "ex5")
 
