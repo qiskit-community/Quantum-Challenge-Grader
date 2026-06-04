a6200ee0eb912106371220f66d523471b6bab09c
Soyoung Shin <63990199+0sophy1@users.noreply.github.com>
2026-06-03 17:09:58 -0700

    Update lab3.py

--- qc_grader/challenges/qgss_2026/lab3.py
+++ qc_grader/challenges/qgss_2026/lab3.py
@@ -81,6 +81,14 @@ def grade_lab3_ex5(
     """
     Grade Exercise 5: Investigate the locality of 3 observables for 15 qubits
     """
+    # convert PauliLindbladMap to sparse lists
+    for key in backward_bound:
+        backward_bound[key] = backward_bound[key].to_sparse_list()
+
+    for i, d in enumerate(forward_list):
+        for key, pl_map in d.items():
+            forward_list[i][key] = pl_map.to_sparse_list()
+
     answer_dict = {
         "circuit_ising": circuit_ising,
         "mirrored_circuit": mirrored_circuit,
