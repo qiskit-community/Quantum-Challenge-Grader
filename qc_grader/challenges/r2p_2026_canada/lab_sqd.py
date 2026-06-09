d65b1ed6bfdf235667caa4ae76e05767f92d698f
Jorge Martinez <Jorge.Martinez@ibm.com>
2026-06-09 15:42:39 +0200

    update copyright

--- qc_grader/challenges/r2p_2026_canada/lab_sqd.py
+++ qc_grader/challenges/r2p_2026_canada/lab_sqd.py
@@ -1,4 +1,4 @@
-# (C) Copyright IBM 2025
+# (C) Copyright IBM 2026
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
